6abed1314f9cc2a110b5014ef534a753d6b864d0
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-06-04 15:21:59 +0300

    conan: add conan files and host_build.sh script
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ conan/conanfile.py
@@ -0,0 +1,15 @@
+from conan import ConanFile
+
+class AosCommonCpp(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("gtest/1.14.0")
+        self.requires("poco/1.13.2")
+        self.requires("grpc/1.54.3")
+        self.requires("libcurl/8.8.0")
+
+    def build_requirements(self):
+        self.tool_requires("protobuf/3.21.12")
+        self.tool_requires("grpc/1.54.3")
