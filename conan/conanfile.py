bc2127581dbad6cb9b349986008f52e071c0de73
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2025-06-12 19:59:55 +0300

    conan: add packages needed by message proxy
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -1,3 +1,4 @@
+import os
 from conan import ConanFile
 
 class AosCommonCpp(ConanFile):
@@ -6,10 +7,20 @@ class AosCommonCpp(ConanFile):
 
     def requirements(self):
         self.requires("gtest/1.14.0")
+        self.requires("libcurl/8.8.0")
         self.requires("poco/1.13.2")
         self.requires("grpc/1.54.3")
-        self.requires("libcurl/8.8.0")
+        self.requires("openssl/3.2.1")
+
+        pkcs11path = os.path.join(self.recipe_folder, "pkcs11provider-1.0.py")
+        self.run("conan export %s --user user --channel stable" % pkcs11path, cwd=self.recipe_folder)
+        self.requires("pkcs11provider/1.0@user/stable")
 
     def build_requirements(self):
         self.tool_requires("protobuf/3.21.12")
         self.tool_requires("grpc/1.54.3")
+        self.tool_requires("gtest/1.14.0")
+
+    def configure(self):
+        self.options["openssl"].no_dso = False
+        self.options["openssl"].shared = True
