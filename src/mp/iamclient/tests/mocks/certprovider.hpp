efd68019c06a81f81f22208dd80937d5ea105dfe
Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
2025-06-15 21:17:16 +0300

    mp:iamclient:tests: remove redundant aos namespace usage
    Signed-off-by: Mykhailo Lohvynenko <Mykhailo_Lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/mp/iamclient/tests/mocks/certprovider.hpp
+++ src/mp/iamclient/tests/mocks/certprovider.hpp
@@ -13,24 +13,26 @@
 
 #include "common/iamclient/publicservicehandler.hpp"
 
-using namespace aos::common::iamclient;
+namespace aos::common::iamclient {
 
 class MockCertProvider : public TLSCredentialsItf {
 public:
-    MOCK_METHOD(aos::RetWithError<std::shared_ptr<grpc::ChannelCredentials>>, GetMTLSClientCredentials,
-        (const aos::String& certStorage), (override));
+    MOCK_METHOD(RetWithError<std::shared_ptr<grpc::ChannelCredentials>>, GetMTLSClientCredentials,
+        (const String& certStorage), (override));
 
-    MOCK_METHOD(aos::RetWithError<std::shared_ptr<grpc::ChannelCredentials>>, GetTLSClientCredentials, (), (override));
+    MOCK_METHOD(RetWithError<std::shared_ptr<grpc::ChannelCredentials>>, GetTLSClientCredentials, (), (override));
 
-    MOCK_METHOD(aos::Error, GetCert,
-        (const aos::String& certType, const aos::Array<uint8_t>& issuer, const aos::Array<uint8_t>& serial,
-            aos::iam::certhandler::CertInfo& certInfo),
+    MOCK_METHOD(Error, GetCert,
+        (const String& certType, const Array<uint8_t>& issuer, const Array<uint8_t>& serial,
+            iam::certhandler::CertInfo& certInfo),
         (const, override));
 
-    MOCK_METHOD(aos::Error, SubscribeCertChanged,
-        (const aos::String& certType, aos::iam::certhandler::CertReceiverItf& subscriber), (override));
+    MOCK_METHOD(Error, SubscribeCertChanged, (const String& certType, iam::certhandler::CertReceiverItf& subscriber),
+        (override));
 
-    MOCK_METHOD(aos::Error, UnsubscribeCertChanged, (aos::iam::certhandler::CertReceiverItf & subscriber), (override));
+    MOCK_METHOD(Error, UnsubscribeCertChanged, (iam::certhandler::CertReceiverItf & subscriber), (override));
 };
+
+} // namespace aos::common::iamclient
 
 #endif
