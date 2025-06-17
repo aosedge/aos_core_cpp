5b8020210649e1f856dd84954b2340407b96eccd
Mykola Kobets <mykola_kobets@epam.com>
2025-06-17 10:21:10 +0300

    cm: add application skeleton
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ src/cm/app/app.hpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CM_APP_APP_HPP_
+#define AOS_CM_APP_APP_HPP_
+
+#include <Poco/Util/ServerApplication.h>
+
+#include <common/logger/logger.hpp>
+
+namespace aos::cm::app {
+
+/**
+ * Aos CM application.
+ */
+class App : public Poco::Util::ServerApplication {
+public:
+    /**
+     * Constructor.
+     */
+    App() = default;
+
+protected:
+    void initialize(Application& self) override;
+    void uninitialize() override;
+    void reinitialize(Application& self) override;
+    int  main(const ArgVec& args) override;
+    void defineOptions(Poco::Util::OptionSet& options) override;
+
+private:
+    static constexpr auto cSDNotifyReady = "READY=1";
+
+    void HandleHelp(const std::string& name, const std::string& value);
+    void HandleConfigFile(const std::string& name, const std::string& value);
+    void HandleLogLevel(const std::string& name, const std::string& value);
+    void HandleReset(const std::string& name, const std::string& value);
+    void HandleVersion(const std::string& name, const std::string& value);
+    void HandleJournal(const std::string& name, const std::string& value);
+
+    void Init();
+    void Start();
+    void Stop();
+
+    common::logger::Logger mLogger;
+    bool                   mStopProcessing = false;
+    bool                   mProvisioning   = false;
+    std::string            mConfigFile;
+};
+
+} // namespace aos::cm::app
+
+#endif
