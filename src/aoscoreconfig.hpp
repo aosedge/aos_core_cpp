55e9db77749846133c62a5d742e2648ea558e677
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-06-26 17:40:56 +0300

    src: cmake: add aoscoreconfig.hpp to override AosCore config items
    Increase size of file path and url up to 512.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

---
+++ src/aoscoreconfig.hpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CORE_CONFIG_HPP_
+#define AOS_CORE_CONFIG_HPP_
+
+/**
+ * File/directory path len.
+ */
+#define AOS_CONFIG_FS_FILE_PATH_LEN 512
+
+/**
+ * URL len.
+ */
+#define AOS_CONFIG_TYPES_URL_LEN 512
+
+#endif
