eed04fffd43a30b7d4f0e78abf02ecc6d951d702
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-07-01 18:07:07 +0300

    docker: conan: remove conan files from docker folder
    As we have all Aos services in one repo, no need to have separate conan files
    for docker and host build.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -1,25 +1,32 @@
 import os
+
 from conan import ConanFile
 
-class AosCommonCpp(ConanFile):
+
+class AosCoreCpp(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
+        self.requires("grpc/1.54.3")
         self.requires("gtest/1.14.0")
         self.requires("libcurl/8.8.0")
-        self.requires("poco/1.13.2")
-        self.requires("grpc/1.54.3")
         self.requires("openssl/3.2.1")
+        self.requires("poco/1.13.2")
 
         pkcs11path = os.path.join(self.recipe_folder, "pkcs11provider-1.0.py")
-        self.run("conan export %s --user user --channel stable" % pkcs11path, cwd=self.recipe_folder)
+
+        self.run(
+            f"conan export {pkcs11path} --user user --channel stable",
+            cwd=self.recipe_folder,
+        )
+
         self.requires("pkcs11provider/1.0@user/stable")
 
     def build_requirements(self):
-        self.tool_requires("protobuf/3.21.12")
         self.tool_requires("grpc/1.54.3")
         self.tool_requires("gtest/1.14.0")
+        self.tool_requires("protobuf/3.21.12")
 
     def configure(self):
         self.options["openssl"].no_dso = False
