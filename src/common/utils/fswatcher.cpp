08beb33b6457036be815e2f371670bf375be041f
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-07-02 15:39:41 +0300

    common: utils: implement file system watcher util
    This patch adds an utility for monitoring file system changes
    using infotify capabilities.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/common/utils/fswatcher.cpp
@@ -0,0 +1,272 @@
+/*
+ * Copyright (C) 2024 Renesas Electronics Corporation.
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <array>
+#include <cerrno>
+#include <cstdlib>
+#include <filesystem>
+#include <numeric>
+#include <string>
+#include <sys/epoll.h>
+#include <sys/inotify.h>
+#include <unistd.h>
+#include <vector>
+
+#include <Poco/UUID.h>
+#include <Poco/UUIDGenerator.h>
+
+#include <aos/common/tools/fs.hpp>
+
+#include <common/logger/logmodule.hpp>
+
+#include "exception.hpp"
+#include "fswatcher.hpp"
+
+namespace aos::common::utils {
+
+/***********************************************************************************************************************
+ * FSWatcher
+ **********************************************************************************************************************/
+
+/***********************************************************************************************************************
+ * Public
+ **********************************************************************************************************************/
+
+Error FSWatcher::Init()
+{
+    LOG_DBG() << "Initialize file system watcher";
+
+    if (mRunning) {
+        return ErrorEnum::eWrongState;
+    }
+
+    mInotifyFd = inotify_init1(IN_NONBLOCK);
+    if (mInotifyFd < 0) {
+        return AOS_ERROR_WRAP(Error(ErrorEnum::eFailed, strerror(errno)));
+    }
+
+    return ErrorEnum::eNone;
+}
+
+Error FSWatcher::Start()
+{
+    std::lock_guard lock {mMutex};
+
+    LOG_DBG() << "Start file system watcher";
+
+    if (mRunning) {
+        return ErrorEnum::eWrongState;
+    }
+
+    mEpollFd = epoll_create1(0);
+    if (mEpollFd < 0) {
+        return Error(ErrorEnum::eFailed, strerror(errno));
+    }
+
+    epoll_event ev {};
+    ev.events  = EPOLLIN;
+    ev.data.fd = mInotifyFd;
+
+    if (epoll_ctl(mEpollFd, EPOLL_CTL_ADD, mInotifyFd, &ev) < 0) {
+        auto err = Error(ErrorEnum::eFailed, strerror(errno));
+
+        close(mEpollFd);
+        mEpollFd = -1;
+
+        return err;
+    }
+
+    mRunning = true;
+
+    mThread = std::thread(&FSWatcher::Run, this);
+
+    return ErrorEnum::eNone;
+}
+
+Error FSWatcher::Stop()
+{
+    {
+        std::lock_guard lock {mMutex};
+
+        LOG_DBG() << "Stop file system watcher";
+
+        if (!mRunning) {
+            return ErrorEnum::eWrongState;
+        }
+
+        ClearWatchedContexts();
+
+        mRunning = false;
+    }
+
+    mCondVar.notify_all();
+
+    if (mThread.joinable()) {
+        mThread.join();
+    }
+
+    return ErrorEnum::eNone;
+}
+
+Error FSWatcher::Subscribe(const std::string& path, FSEventSubscriber& subscriber)
+{
+    std::lock_guard lock {mMutex};
+
+    LOG_DBG() << "Start watching" << Log::Field("path", path.c_str());
+
+    if (mWatchContexts.find(path) == mWatchContexts.end()) {
+        int wd = inotify_add_watch(mInotifyFd, path.c_str(), mFlags);
+        if (wd < 0) {
+            return AOS_ERROR_WRAP(Error(ErrorEnum::eFailed, strerror(errno)));
+        }
+
+        mWatchContexts[path].mFD   = wd;
+        mWatchContexts[path].mMask = mFlags;
+    }
+
+    auto& subscribers = mWatchContexts[path].mSubscribers;
+
+    if (std::find(subscribers.begin(), subscribers.end(), &subscriber) != subscribers.end()) {
+        return ErrorEnum::eAlreadyExist;
+    }
+
+    subscribers.push_back(&subscriber);
+
+    return ErrorEnum::eNone;
+}
+
+Error FSWatcher::Unsubscribe(const std::string& path, FSEventSubscriber& subscriber)
+{
+    std::lock_guard lock {mMutex};
+
+    LOG_DBG() << "Unsubscribe fs event subscriber" << Log::Field("path", path.c_str());
+
+    auto it = mWatchContexts.find(path);
+    if (it == mWatchContexts.end()) {
+        return ErrorEnum::eNotFound;
+    }
+
+    auto& pathSubscribers = it->second.mSubscribers;
+
+    pathSubscribers.erase(
+        std::remove(pathSubscribers.begin(), pathSubscribers.end(), &subscriber), pathSubscribers.end());
+
+    if (!pathSubscribers.empty()) {
+        return ErrorEnum::eNone;
+    }
+
+    if (inotify_rm_watch(mInotifyFd, it->second.mFD) < 0) {
+        LOG_ERR() << AOS_ERROR_WRAP(Error(ErrorEnum::eFailed, strerror(errno)));
+    }
+
+    mWatchContexts.erase(it);
+
+    return ErrorEnum::eNone;
+}
+
+FSWatcher::~FSWatcher()
+{
+    LOG_DBG() << "Destroy file system watcher";
+
+    ClearWatchedContexts();
+
+    if (mInotifyFd >= 0) {
+        close(mInotifyFd);
+    }
+}
+
+/***********************************************************************************************************************
+ * Private
+ **********************************************************************************************************************/
+
+void FSWatcher::Run()
+{
+    constexpr auto cItemSize = sizeof(struct inotify_event) + cFilePathLen + 1;
+
+    std::vector<char> buffer(cItemSize * mMaxPollEvents);
+    epoll_event       events = {};
+
+    while (true) {
+        {
+            std::lock_guard lock {mMutex};
+
+            if (!mRunning) {
+                return;
+            }
+        }
+
+        const auto waitResult
+            = epoll_wait(mEpollFd, &events, mMaxPollEvents, static_cast<int>(mPollTimeout.Milliseconds()));
+        if (waitResult < 0) {
+            if (errno != EINTR) {
+                LOG_ERR() << "Wait poll event failed" << Log::Field(Error(errno));
+            }
+
+            continue;
+        } else if (waitResult == 0) {
+            continue;
+        }
+
+        const auto length = read(mInotifyFd, buffer.data(), buffer.size());
+        if (length <= 0) {
+            continue;
+        }
+
+        int i = 0;
+
+        while (i < length) {
+            struct inotify_event* event = reinterpret_cast<struct inotify_event*>(&buffer[i]);
+
+            i += sizeof(struct inotify_event) + event->len;
+
+            std::lock_guard lock {mMutex};
+
+            auto it = std::find_if(mWatchContexts.begin(), mWatchContexts.end(),
+                [event](const auto& pair) { return pair.second.mFD == event->wd; });
+
+            if (it == mWatchContexts.end() || !(it->second.mMask & mFlags)) {
+                continue;
+            }
+
+            for (auto* subscriber : it->second.mSubscribers) {
+                if (subscriber == nullptr) {
+                    continue;
+                }
+
+                subscriber->OnFSEvent(it->first, event->mask);
+            }
+        }
+    }
+}
+
+void FSWatcher::ClearWatchedContexts()
+{
+    if (!mRunning) {
+        return;
+    }
+
+    for (const auto& [path, context] : mWatchContexts) {
+        if (context.mFD < 0) {
+            continue;
+        }
+
+        if (inotify_rm_watch(mInotifyFd, context.mFD) < 0) {
+            LOG_WRN() << "Failed to remove inotify watch" << Log::Field("path", path.c_str())
+                      << Log::Field(Error(errno));
+        }
+    }
+
+    mWatchContexts.clear();
+
+    if (mEpollFd >= 0) {
+        close(mEpollFd);
+
+        mEpollFd = -1;
+    }
+}
+
+} // namespace aos::common::utils
---
+++ src/common/utils/fswatcher.hpp
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_COMMON_UTILS_FSWATCHER_HPP_
+#define AOS_COMMON_UTILS_FSWATCHER_HPP_
+
+#include <condition_variable>
+#include <functional>
+#include <mutex>
+#include <string>
+#include <sys/inotify.h>
+#include <thread>
+#include <unordered_map>
+
+#include <aos/common/tools/error.hpp>
+#include <aos/common/tools/time.hpp>
+
+namespace aos::common::utils {
+
+class FSEventSubscriber {
+public:
+    /**
+     * Called when file system event occurs for a specified path.
+     *
+     * @param path path to the file or directory that triggered the event.
+     * @param mask bitmask of events that occurred, e.g., IN_MODIFY, IN_CREATE, IN_DELETE.
+     */
+    virtual void OnFSEvent(const std::string& path, uint32_t mask) = 0;
+
+    /**
+     * Destructor.
+     */
+    virtual ~FSEventSubscriber() = default;
+};
+
+/**
+ * File system watcher.
+ */
+class FSWatcher : public NonCopyable {
+public:
+    /**
+     * Constructs file system watcher.
+     *
+     * @param pollTimeout poll timeout duration.
+     * @param maxPollEvents maximum number of poll events to process in one iteration.
+     */
+    explicit FSWatcher(Duration pollTimeout = Time::cSeconds * 5, size_t maxPollEvents = 1, size_t flags = IN_MODIFY)
+        : mPollTimeout(pollTimeout)
+        , mMaxPollEvents(maxPollEvents)
+        , mFlags(flags)
+    {
+    }
+
+    /**
+     * Initializes file system watcher.
+     *
+     * @return Error.
+     */
+    Error Init();
+
+    /**
+     * Starts file system watcher.
+     *
+     * @return Error.
+     */
+    Error Start();
+
+    /**
+     * Stops file system watcher.
+     *
+     * @return Error.
+     */
+    Error Stop();
+
+    /**
+     * Subscribes subscriber on fs events for the specified path.
+     *
+     * @param path path to watch.
+     * @param subscriber subscriber object.
+     * @return Error.
+     */
+    Error Subscribe(const std::string& path, FSEventSubscriber& subscriber);
+
+    /**
+     * Unsubscribes subscriber.
+     *
+     * @param path path to unsubscribe from.
+     * @param subscriber subscriber to unsubscribe.
+     * @return Error.
+     */
+    Error Unsubscribe(const std::string& path, FSEventSubscriber& subscriber);
+
+    /**
+     * Destructor.
+     */
+    ~FSWatcher();
+
+private:
+    void Run();
+    void ClearWatchedContexts();
+
+    struct Context {
+        int                             mFD = -1;
+        std::vector<FSEventSubscriber*> mSubscribers;
+        uint32_t                        mMask = 0;
+    };
+
+    Duration                                 mPollTimeout;
+    size_t                                   mMaxPollEvents = 1;
+    size_t                                   mFlags         = IN_MODIFY;
+    int                                      mInotifyFd     = -1;
+    int                                      mEpollFd       = -1;
+    bool                                     mRunning       = false;
+    std::thread                              mThread;
+    std::mutex                               mMutex;
+    std::condition_variable                  mCondVar;
+    std::unordered_map<std::string, Context> mWatchContexts;
+};
+
+} // namespace aos::common::utils
+
+#endif
