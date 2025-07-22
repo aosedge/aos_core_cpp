ce2c18f3ad46bb0007d88f3a8d5bebd7760935dd
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-07-22 14:24:53 +0300

    common: cloudprotocol: convert common cloud protocol types
    This patch adds the common cloud protocol types conversions
    to and from JSON.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/common/cloudprotocol/common.cpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <Poco/JSON/Parser.h>
+
+#include <common/utils/exception.hpp>
+#include <common/utils/json.hpp>
+#include <common/utils/time.hpp>
+
+#include "common.hpp"
+
+namespace aos::common::cloudprotocol {
+
+Error FromJSON(const utils::CaseInsensitiveObjectWrapper& json, Error& error)
+{
+    if (auto code = json.GetValue<int>("aosCode", 0); code > 0) {
+        error = Error(static_cast<Error::Enum>(code), json.GetValue<std::string>("message", "").c_str());
+    }
+
+    return ErrorEnum::eNone;
+}
+
+Error ToJSON(const Error& error, Poco::JSON::Object& json)
+{
+    json.set("aosCode", static_cast<int>(error.Value()));
+    json.set("errno", error.Errno());
+    json.set("message", error.Message());
+
+    return ErrorEnum::eNone;
+}
+
+Error FromJSON(const utils::CaseInsensitiveObjectWrapper& json, InstanceIdent& instanceIdent)
+{
+    if (auto err = instanceIdent.mServiceID.Assign(json.GetValue<std::string>("serviceID").c_str()); !err.IsNone()) {
+        return Error(err, "serviceID parsing failed");
+    }
+
+    if (auto err = instanceIdent.mSubjectID.Assign(json.GetValue<std::string>("subjectID").c_str()); !err.IsNone()) {
+        return Error(err, "subjectID parsing failed");
+    }
+
+    instanceIdent.mInstance = json.GetValue<uint64_t>("instance");
+
+    return ErrorEnum::eNone;
+}
+
+Error ToJSON(const InstanceIdent& instanceIdent, Poco::JSON::Object& json)
+{
+    json.set("serviceID", instanceIdent.mServiceID.CStr());
+    json.set("subjectID", instanceIdent.mSubjectID.CStr());
+    json.set("instance", instanceIdent.mInstance);
+
+    return ErrorEnum::eNone;
+}
+
+Error FromJSON(const utils::CaseInsensitiveObjectWrapper& json, aos::cloudprotocol::InstanceFilter& instanceFilter)
+{
+    if (json.Has("serviceID")) {
+        instanceFilter.mServiceID.EmplaceValue();
+
+        if (auto err = instanceFilter.mServiceID.GetValue().Assign(json.GetValue<std::string>("serviceID").c_str());
+            !err.IsNone()) {
+            return AOS_ERROR_WRAP(Error(err, "failed to parse serviceID"));
+        }
+    }
+
+    if (json.Has("subjectID")) {
+        instanceFilter.mSubjectID.EmplaceValue();
+
+        if (auto err = instanceFilter.mSubjectID.GetValue().Assign(json.GetValue<std::string>("subjectID").c_str());
+            !err.IsNone()) {
+            return AOS_ERROR_WRAP(Error(err, "failed to parse subjectID"));
+        }
+    }
+
+    if (json.Has("instance")) {
+        instanceFilter.mInstance.EmplaceValue(json.GetValue<uint64_t>("instance"));
+    }
+
+    return ErrorEnum::eNone;
+}
+
+Error ToJSON(const aos::cloudprotocol::InstanceFilter& instanceFilter, Poco::JSON::Object& json)
+{
+    try {
+        if (instanceFilter.mServiceID.HasValue()) {
+            json.set("serviceID", instanceFilter.mServiceID.GetValue().CStr());
+        }
+
+        if (instanceFilter.mSubjectID.HasValue()) {
+            json.set("subjectID", instanceFilter.mSubjectID.GetValue().CStr());
+        }
+
+        if (instanceFilter.mInstance.HasValue()) {
+            json.set("instance", instanceFilter.mInstance.GetValue());
+        }
+    } catch (const std::exception& e) {
+        return utils::ToAosError(e);
+    }
+
+    return ErrorEnum::eNone;
+}
+
+} // namespace aos::common::cloudprotocol
