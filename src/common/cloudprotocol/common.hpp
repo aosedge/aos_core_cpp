6e2d6a8cf8e6e338fe69adc30b22e14716b114ff
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-07-22 14:24:53 +0300

    common: cloudprotocol: convert common cloud protocol types
    This patch adds the common cloud protocol types conversions
    to and from JSON.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/common/cloudprotocol/common.hpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_COMMON_CLOUDPROTOCOL_COMMON_HPP_
+#define AOS_COMMON_CLOUDPROTOCOL_COMMON_HPP_
+
+#include <variant>
+
+#include <Poco/JSON/Object.h>
+
+#include <aos/common/cloudprotocol/cloudprotocol.hpp>
+
+#include <common/utils/json.hpp>
+
+namespace aos::common::cloudprotocol {
+
+/**
+ * Converts JSON object to Error object.
+ *
+ * @param json json object representation.
+ * @param[out] error Error object to fill.
+ * @return Error.
+ */
+Error FromJSON(const utils::CaseInsensitiveObjectWrapper& json, Error& error);
+
+/**
+ * Converts Error object to JSON object.
+ *
+ * @param error Error object to convert.
+ * @param[out] json JSON object to fill.
+ * @return Error.
+ */
+Error ToJSON(const Error& error, Poco::JSON::Object& json);
+
+/**
+ * Converts JSON object to InstanceIdent object.
+ *
+ * @param json json object representation.
+ * @param[out] instanceIdent instance ident object to fill.
+ * @return Error.
+ */
+Error FromJSON(const utils::CaseInsensitiveObjectWrapper& json, InstanceIdent& instanceIdent);
+
+/**
+ * Converts InstanceIdent object to JSON object.
+ *
+ * @param instanceIdent instance ident object to convert.
+ * @param[out] json JSON object to fill.
+ * @return Error.
+ */
+Error ToJSON(const InstanceIdent& instanceIdent, Poco::JSON::Object& json);
+
+/**
+ * Converts JSON object to InstanceFilter object.
+ *
+ * @param json json object representation.
+ * @param[out] instanceFilter instance filter object to fill.
+ * @return Error.
+ */
+Error FromJSON(const utils::CaseInsensitiveObjectWrapper& json, aos::cloudprotocol::InstanceFilter& instanceFilter);
+
+/**
+ * Converts InstanceFilter object to JSON object.
+ *
+ * @param instanceFilter instance filter object to convert.
+ * @param[out] json JSON object to fill.
+ * @return Error.
+ */
+Error ToJSON(const aos::cloudprotocol::InstanceFilter& instanceFilter, Poco::JSON::Object& json);
+
+} // namespace aos::common::cloudprotocol
+
+#endif
