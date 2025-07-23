9fd7e4d3f81068f272d10566301ee712719dffa2
Mykola Solianko <mykola_solianko@epam.com>
2025-07-23 20:59:42 +0300

    cm: networkmanager: add UT
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/cm/networkmanager/tests/mocks/randommock.hpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CM_NETWORKMANAGER_TESTS_MOCKS_RANDOMMOCK_HPP_
+#define AOS_CM_NETWORKMANAGER_TESTS_MOCKS_RANDOMMOCK_HPP_
+
+#include <gmock/gmock.h>
+
+#include <core/common/crypto/crypto.hpp>
+
+namespace aos::crypto {
+
+class MockRandom : public RandomItf {
+public:
+    MOCK_METHOD(RetWithError<uint64_t>, RandInt, (uint64_t max), (override));
+    MOCK_METHOD(Error, RandBuffer, (Array<uint8_t> & buffer, size_t size), (override));
+};
+
+} // namespace aos::crypto
+
+#endif
