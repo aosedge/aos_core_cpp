2afac2c80ba11129b76fd155a13f027f2fdaf31f
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-07-29 13:58:22 +0300

    common: cloudprotocol: convert desired status cloud protocol types
    This patch adds desired status cloud protocol types conversions
    to and from JSON.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/common/cloudprotocol/desiredstatus.hpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_COMMON_CLOUDPROTOCOL_DESIREDSTATUS_HPP_
+#define AOS_COMMON_CLOUDPROTOCOL_DESIREDSTATUS_HPP_
+
+#include <variant>
+
+#include <Poco/JSON/Object.h>
+
+#include <aos/common/cloudprotocol/desiredstatus.hpp>
+
+#include <common/utils/json.hpp>
+
+namespace aos::common::cloudprotocol {
+
+/**
+ * Converts JSON object to node config object.
+ *
+ * @param json json object representation.
+ * @param[out] nodeConfig node config object to fill.
+ * @return Error.
+ */
+Error FromJSON(const utils::CaseInsensitiveObjectWrapper& json, aos::cloudprotocol::NodeConfig& nodeConfig);
+
+/**
+ * Converts node config object to JSON object.
+ *
+ * @param nodeConfig node config object to convert.
+ * @param[out] json JSON object to fill.
+ * @return Error.
+ */
+Error ToJSON(const aos::cloudprotocol::NodeConfig& nodeConfig, Poco::JSON::Object& json);
+
+/**
+ * Converts JSON object to desired status object.
+ *
+ * @param json json object representation.
+ * @param[out] desiredStatus desired status object to fill.
+ * @return Error.
+ */
+Error FromJSON(const utils::CaseInsensitiveObjectWrapper& json, aos::cloudprotocol::DesiredStatus& desiredStatus);
+
+/**
+ * Converts DeltaUnitStatus object to JSON object.
+ *
+ * @param deltaUnitStatus delta unit status object to convert.
+ * @param[out] json JSON object to fill.
+ * @return Error.
+ */
+Error ToJSON(const aos::cloudprotocol::DesiredStatus& desiredStatus, Poco::JSON::Object& json);
+
+} // namespace aos::common::cloudprotocol
+
+#endif
