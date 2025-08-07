e1a822d5bb150121d54f093aade49729a3a98779
Mykola Solianko <mykola_solianko@epam.com>
2025-08-07 19:32:13 +0300

    common: utils: add hash for InstanceIdent
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/common/utils/utils.hpp
+++ src/common/utils/utils.hpp
@@ -11,6 +11,38 @@
 #include <vector>
 
 #include <core/common/tools/error.hpp>
+#include <core/common/types/types.hpp>
+
+/***********************************************************************************************************************
+ * std namespace
+ **********************************************************************************************************************/
+
+namespace std {
+
+/**
+ * Hash instance identifier.
+ */
+template <>
+struct hash<aos::InstanceIdent> {
+    size_t operator()(aos::InstanceIdent const& id) const noexcept
+    {
+        std::string_view s1 {id.mItemID.CStr()};
+        std::string_view s2 {id.mSubjectID.CStr()};
+
+        size_t h1 = std::hash<std::string_view> {}(s1);
+        size_t h2 = std::hash<std::string_view> {}(s2);
+        size_t h3 = std::hash<uint64_t> {}(id.mInstance);
+
+        size_t seed = h1;
+
+        seed ^= h2 + 0x9e3779b97f4a7c15ULL + (seed << 6) + (seed >> 2);
+        seed ^= h3 + 0x9e3779b97f4a7c15ULL + (seed << 6) + (seed >> 2);
+
+        return seed;
+    }
+};
+
+} // namespace std
 
 namespace aos::common::utils {
 
