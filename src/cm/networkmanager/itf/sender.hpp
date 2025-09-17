8165cdff880a2a047a058675b2bcba8e36fecc53
Mykola Solianko <mykola_solianko@epam.com>
2025-09-17 09:45:22 +0300

    cm: networkmanager: add network sender itf
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/cm/networkmanager/itf/sender.hpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CM_NETWORKMANAGER_ITF_SENDER_HPP_
+#define AOS_CM_NETWORKMANAGER_ITF_SENDER_HPP_
+
+#include <vector>
+
+#include <core/common/types/types.hpp>
+
+namespace aos::cm::networkmanager {
+
+/**
+ * Sender interface.
+ */
+class SenderItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~SenderItf() = default;
+
+    /**
+     * Sends network.
+     *
+     * @param nodeID Node ID.
+     * @param networkParameters Network parameters.
+     * @return Error.
+     */
+    virtual Error SendNetwork(const std::string& nodeID, const std::vector<NetworkParameters>& networkParameters) = 0;
+};
+
+} // namespace aos::cm::networkmanager
+
+#endif // AOS_CM_NETWORKMANAGER_ITF_SENDER_HPP_
