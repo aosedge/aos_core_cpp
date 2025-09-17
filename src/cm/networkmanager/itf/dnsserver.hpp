2f861aef017cb564ce08576c58f024b242512ae7
Mykola Solianko <mykola_solianko@epam.com>
2025-09-17 09:45:58 +0300

    cm: networkmanager: add dnsserver itf
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/cm/networkmanager/itf/dnsserver.hpp
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CM_NETWORKMANAGER_ITF_DNSSERVER_HPP_
+#define AOS_CM_NETWORKMANAGER_ITF_DNSSERVER_HPP_
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <core/common/types/types.hpp>
+
+namespace aos::cm::networkmanager {
+
+using HostsMap = std::unordered_map<std::string, std::vector<std::string>>;
+
+/**
+ * DNS server interface.
+ */
+class DNSServerItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~DNSServerItf() = default;
+
+    /**
+     * Updates hosts file.
+     *
+     * @param hosts Hosts.
+     * @return Error.
+     */
+    virtual Error UpdateHostsFile(const HostsMap& hosts) = 0;
+
+    /**
+     * Restarts DNS server.
+     *
+     * @return Error.
+     */
+    virtual Error Restart() = 0;
+
+    /**
+     * Gets IP.
+     *
+     * @return IP.
+     */
+    virtual std::string GetIP() const = 0;
+};
+
+} // namespace aos::cm::networkmanager
+
+#endif // AOS_CM_NETWORKMANAGER_ITF_DNSSERVER_HPP_
