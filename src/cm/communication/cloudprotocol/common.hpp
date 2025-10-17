c0ec082992bf739281ef9d2bac24d1423c268549
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-10-17 14:51:13 +0300

    cm: communication: cloudprotocol: add common utils
    This patch adds common utilities for the cloud protocol
    message parsing/unparsing.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/cm/communication/cloudprotocol/common.hpp
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CM_COMMUNICATION_CLOUDPROTOCOL_COMMON_HPP_
+#define AOS_CM_COMMUNICATION_CLOUDPROTOCOL_COMMON_HPP_
+
+#include <Poco/JSON/Object.h>
+
+#include <core/common/types/common.hpp>
+
+#include <common/utils/json.hpp>
+
+namespace aos::cm::communication::cloudprotocol {
+
+/**
+ * Message type.
+ */
+class MessageTypeType {
+public:
+    enum class Enum {
+        eAlerts,
+        eDeprovisioningRequest,
+        eDeprovisioningResponse,
+        eDesiredStatus,
+        eFinishProvisioningRequest,
+        eFinishProvisioningResponse,
+        eInstallUnitCertificatesConfirmation,
+        eIssuedUnitCertificates,
+        eIssueUnitCertificates,
+        eMonitoringData,
+        eNewState,
+        eOverrideEnvVars,
+        eOverrideEnvVarsStatus,
+        ePushLog,
+        eRenewCertificatesNotification,
+        eRequestLog,
+        eStartProvisioningRequest,
+        eStartProvisioningResponse,
+        eStateAcceptance,
+        eStateRequest,
+        eUnitStatus,
+        eUpdateState,
+    };
+
+    static const Array<const char* const> GetStrings()
+    {
+        static const char* const sStrings[] = {
+            "alerts",
+            "deprovisioningRequest",
+            "deprovisioningResponse",
+            "desiredStatus",
+            "finishProvisioningRequest",
+            "finishProvisioningResponse",
+            "installUnitCertificatesConfirmation",
+            "issuedUnitCertificates",
+            "issueUnitCertificates",
+            "monitoringData",
+            "newState",
+            "overrideEnvVars",
+            "overrideEnvVarsStatus",
+            "pushLog",
+            "renewCertificatesNotification",
+            "requestLog",
+            "startProvisioningRequest",
+            "startProvisioningResponse",
+            "stateAcceptance",
+            "stateRequest",
+            "unitStatus",
+            "updateState",
+        };
+
+        return Array<const char* const>(sStrings, ArraySize(sStrings));
+    };
+};
+
+using MessageTypeEnum = MessageTypeType::Enum;
+using MessageType     = EnumStringer<MessageTypeType>;
+
+/**
+ * Converts Error object to JSON object.
+ *
+ * @param error Error object to convert.
+ * @param[out] json JSON object to fill.
+ * @return Error.
+ */
+Error ToJSON(const Error& error, Poco::JSON::Object& json);
+
+/**
+ * Creates AosIdentity JSON object.
+ *
+ * @param id id to convert.
+ * @param type type to convert.
+ * @return Poco::JSON::Object::Ptr.
+ */
+Poco::JSON::Object::Ptr CreateAosIdentity(const Optional<String>& id, const Optional<UpdateItemType>& type = {});
+
+/**
+ * Parses id from AosIdentity JSON object.
+ *
+ * @param json json object representation.
+ * @param[out] id id to fill.
+ * @return Error.
+ */
+Error ParseAosIdentityID(const common::utils::CaseInsensitiveObjectWrapper& json, String& id);
+
+/**
+ * Converts InstanceIdent object to JSON.
+ *
+ * @param instanceIdent InstanceIdent object to convert.
+ * @param[out] json JSON object to fill.
+ * @return Error.
+ */
+Error ToJSON(const InstanceIdent& instanceIdent, Poco::JSON::Object& json);
+
+/**
+ * Converts JSON object to InstanceIdent.
+ *
+ * @param json JSON object to convert.
+ * @param[out] instanceIdent InstanceIdent object to fill.
+ * @return Error.
+ */
+Error FromJSON(const common::utils::CaseInsensitiveObjectWrapper& json, InstanceIdent& instanceIdent);
+
+/**
+ * Converts JSON object to InstanceFilter.
+ *
+ * @param json JSON object to convert.
+ * @param[out] instanceFilter InstanceFilter object to fill.
+ * @return Error.
+ */
+Error FromJSON(const common::utils::CaseInsensitiveObjectWrapper& json, InstanceFilter& instanceFilter);
+
+} // namespace aos::cm::communication::cloudprotocol
+
+#endif
