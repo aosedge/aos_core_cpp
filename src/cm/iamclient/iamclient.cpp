b552f9c29c3184439d08f5c1f94dadab07673836
Mykola Solianko <mykola_solianko@epam.com>
2025-11-05 18:30:40 +0200

    cm: iamclient: add implementation
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

---
+++ src/cm/iamclient/iamclient.cpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "iamclient.hpp"
+
+#include <core/common/tools/log.hpp>
+
+namespace aos::cm::iamclient {
+
+IAMClient::~IAMClient()
+{
+    PublicCertService::UnsubscribeListener(*this);
+}
+
+Error IAMClient::Init(const std::string& iamProtectedServerURL, const std::string& iamPublicServerURL,
+    const std::string& certStorage, aos::common::iamclient::TLSCredentialsItf& tlsCredentials, const String& certType,
+    bool insecureConnection)
+{
+    LOG_INF() << "Initializing IAM client";
+
+    auto err = CertificateService::Init(iamProtectedServerURL, certStorage, tlsCredentials, insecureConnection);
+    if (!err.IsNone()) {
+        return err;
+    }
+
+    err = NodesService::Init(iamProtectedServerURL, certStorage, tlsCredentials, insecureConnection);
+    if (!err.IsNone()) {
+        return err;
+    }
+
+    err = ProvisioningService::Init(iamProtectedServerURL, certStorage, tlsCredentials, insecureConnection);
+    if (!err.IsNone()) {
+        return err;
+    }
+
+    err = PublicCertService::Init(iamPublicServerURL, tlsCredentials, insecureConnection);
+    if (!err.IsNone()) {
+        return err;
+    }
+
+    err = PublicNodesService::Init(iamPublicServerURL, tlsCredentials, insecureConnection);
+    if (!err.IsNone()) {
+        return err;
+    }
+
+    err = PublicCertService::SubscribeListener(certType, *this);
+    if (!err.IsNone()) {
+        return err;
+    }
+
+    LOG_INF() << "IAM client initialized successfully";
+
+    return ErrorEnum::eNone;
+}
+
+void IAMClient::OnCertChanged([[maybe_unused]] const CertInfo& info)
+{
+    LOG_INF() << "Certificate changed, reconnect all services";
+
+    auto err = CertificateService::Reconnect();
+    if (!err.IsNone()) {
+        LOG_ERR() << "Failed to reconnect certificate service" << Log::Field(err);
+    }
+
+    err = NodesService::Reconnect();
+    if (!err.IsNone()) {
+        LOG_ERR() << "Failed to reconnect nodes service" << Log::Field(err);
+    }
+
+    err = ProvisioningService::Reconnect();
+    if (!err.IsNone()) {
+        LOG_ERR() << "Failed to reconnect provisioning service" << Log::Field(err);
+    }
+
+    err = PublicCertService::Reconnect();
+    if (!err.IsNone()) {
+        LOG_ERR() << "Failed to reconnect public cert service" << Log::Field(err);
+    }
+
+    err = PublicNodesService::Reconnect();
+    if (!err.IsNone()) {
+        LOG_ERR() << "Failed to reconnect public nodes service" << Log::Field(err);
+    }
+}
+
+} // namespace aos::cm::iamclient
---
+++ src/cm/iamclient/iamclient.hpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_CM_IAMCLIENT_IAMCLIENT_HPP_
+#define AOS_CM_IAMCLIENT_IAMCLIENT_HPP_
+
+#include <common/iamclient/certificateservice.hpp>
+#include <common/iamclient/nodesservice.hpp>
+#include <common/iamclient/provisioningservice.hpp>
+#include <common/iamclient/publiccertservice.hpp>
+#include <common/iamclient/publicnodeservice.hpp>
+
+#include <core/common/iamclient/itf/certprovider.hpp>
+#include <core/common/tools/error.hpp>
+
+namespace aos::cm::iamclient {
+
+/**
+ * IAM client that aggregates IAM services used by IAMClientItf.
+ */
+class IAMClient : public aos::common::iamclient::CertificateService,
+                  public aos::common::iamclient::NodesService,
+                  public aos::common::iamclient::ProvisioningService,
+                  public aos::common::iamclient::PublicCertService,
+                  public aos::common::iamclient::PublicNodesService,
+                  public aos::iamclient::CertListenerItf {
+public:
+    /**
+     * Destructor.
+     */
+    ~IAMClient();
+
+    /**
+     * Initializes IAM client.
+     *
+     * @param iamProtectedServerURL IAM protected server URL.
+     * @param iamPublicServerURL IAM public server URL.
+     * @param certStorage certificate storage.
+     * @param tlsCredentials TLS credentials.
+     * @param certType certificate type to subscribe for updates.
+     * @param insecureConnection use insecure connection.
+     * @return Error.
+     */
+    Error Init(const std::string& iamProtectedServerURL, const std::string& iamPublicServerURL,
+        const std::string& certStorage, aos::common::iamclient::TLSCredentialsItf& tlsCredentials,
+        const String& certType, bool insecureConnection = false);
+
+    /**
+     * Reconnects all services.
+     *
+     * @param info certificate info.
+     */
+    void OnCertChanged(const CertInfo& info) override;
+};
+
+} // namespace aos::cm::iamclient
+
+#endif
