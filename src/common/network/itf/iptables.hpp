15c96b6de032d90fe6bf479b912ec6c3f94e2797
Mykola Solianko <mykola_solianko@epam.com>
2025-11-13 13:49:52 +0200

    common: network: move IPTablesItf and RuleBuilder to itf folder
    Move IPTablesItf interface and RuleBuilder class from iptables.hpp to
    dedicated itf/iptables.hpp file to follow the project's interface
    organization pattern.
    
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

---
+++ src/common/network/itf/iptables.hpp
@@ -0,0 +1,168 @@
+/*
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_COMMON_NETWORK_ITF_IPTABLES_HPP_
+#define AOS_COMMON_NETWORK_ITF_IPTABLES_HPP_
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include <core/common/tools/error.hpp>
+
+namespace aos::common::network {
+
+/**
+ * Builder for iptables rule.
+ */
+class RuleBuilder {
+public:
+    /**
+     * Sets source address.
+     *
+     * @param addr source address.
+     */
+    RuleBuilder& Source(const std::string& addr);
+
+    /**
+     * Sets destination address.
+     *
+     * @param addr destination address.
+     */
+    RuleBuilder& Destination(const std::string& addr);
+
+    /**
+     * Sets protocol.
+     *
+     * @param proto protocol.
+     */
+    RuleBuilder& Protocol(const std::string& proto);
+
+    /**
+     * Sets jump target.
+     *
+     * @param target jump target.
+     */
+    RuleBuilder& Jump(const std::string& target);
+
+    /**
+     * Sets source port.
+     *
+     * @param port source port.
+     */
+    RuleBuilder& SourcePort(uint16_t port);
+
+    /**
+     * Sets destination port.
+     *
+     * @param port destination port.
+     */
+    RuleBuilder& DestinationPort(uint16_t port);
+
+    /**
+     * Builds rule string.
+     *
+     * @return rule string.
+     */
+    std::string Build() const;
+
+    /**
+     * Resets rule builder.
+     */
+    void Reset();
+
+private:
+    std::string mRule;
+};
+
+/**
+ * Interface for iptables.
+ */
+class IPTablesItf {
+public:
+    /**
+     * Destructor.
+     */
+    virtual ~IPTablesItf() = default;
+
+    /**
+     * Appends rule to the chain.
+     *
+     * @param chain chain name.
+     * @param builder rule builder.
+     * @return error.
+     */
+    virtual Error Append(const std::string& chain, const RuleBuilder& builder) = 0;
+
+    /**
+     * Inserts rule to the chain at specified position.
+     *
+     * @param chain chain name.
+     * @param position position.
+     * @param builder rule builder.
+     * @return error.
+     */
+    virtual Error Insert(const std::string& chain, unsigned int position, const RuleBuilder& builder) = 0;
+
+    /**
+     * Deletes rule from the chain.
+     *
+     * @param chain chain name.
+     * @param builder rule builder.
+     * @return error.
+     */
+    virtual Error DeleteRule(const std::string& chain, const RuleBuilder& builder) = 0;
+
+    /**
+     * Creates new chain.
+     *
+     * @param chain chain name.
+     * @return error.
+     */
+    virtual Error NewChain(const std::string& chain) = 0;
+
+    /**
+     * Clears chain.
+     *
+     * @param chain chain name.
+     * @return error.
+     */
+    virtual Error ClearChain(const std::string& chain) = 0;
+
+    /**
+     * Deletes chain.
+     *
+     * @param chain chain name.
+     * @return error.
+     */
+    virtual Error DeleteChain(const std::string& chain) = 0;
+
+    /**
+     * Lists all chains.
+     *
+     * @return list of chains.
+     */
+    virtual RetWithError<std::vector<std::string>> ListChains() = 0;
+
+    /**
+     * Lists all rules with counters.
+     *
+     * @param chain chain name.
+     * @return list of rules with counters.
+     */
+    virtual RetWithError<std::vector<std::string>> ListAllRulesWithCounters(const std::string& chain) = 0;
+
+    /**
+     * Creates new rule builder.
+     *
+     * @return new rule builder.
+     */
+    RuleBuilder CreateRule() { return RuleBuilder(); }
+};
+
+} // namespace aos::common::network
+
+#endif // AOS_COMMON_NETWORK_ITF_IPTABLES_HPP_
