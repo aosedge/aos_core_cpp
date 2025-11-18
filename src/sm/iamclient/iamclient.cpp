88ded569b3718df379cd4a66e3179b6583937d97
Mykola Solianko <mykola_solianko@epam.com>
2025-11-18 09:44:39 +0200

    sm: iamclient: add implementation
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

---
+++ src/sm/iamclient/iamclient.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <core/common/tools/log.hpp>
+
+#include "iamclient.hpp"
+
+namespace aos::sm::iamclient {
+
+IAMClient::~IAMClient()
+{
+    PublicCertService::UnsubscribeListener(*this);
+}
+
+Error IAMClient::Init(const std::string& iamProtectedServerURL, const std::string& iamPublicServerURL,
+    const std::string& certStorage, aos::common::iamclient::TLSCredentialsItf& tlsCredentials, const String& certType,
+    bool insecureConnection)
+{
+    LOG_INF() << "Initializing IAM client";
+
+    auto err = PermissionsService::Init(iamProtectedServerURL, certStorage, tlsCredentials, insecureConnection);
+    if (!err.IsNone()) {
+        return err;
+    }
+
+    err = PublicCertService::Init(iamPublicServerURL, tlsCredentials, insecureConnection);
+    if (!err.IsNone()) {
+        return err;
+    }
+
+    err = PublicCurrentNodeService::Init(iamPublicServerURL, tlsCredentials, insecureConnection);
+    if (!err.IsNone()) {
+        return err;
+    }
+
+    err = PublicCertService::SubscribeListener(certType, *this);
+    if (!err.IsNone()) {
+        return err;
+    }
+
+    LOG_INF() << "IAM client initialized successfully";
+
+    return ErrorEnum::eNone;
+}
+
+void IAMClient::OnCertChanged([[maybe_unused]] const CertInfo& info)
+{
+    LOG_INF() << "Certificate changed, reconnect all services";
+
+    auto err = PermissionsService::Reconnect();
+    if (!err.IsNone()) {
+        LOG_ERR() << "Failed to reconnect permissions service" << Log::Field(err);
+    }
+
+    err = PublicCertService::Reconnect();
+    if (!err.IsNone()) {
+        LOG_ERR() << "Failed to reconnect public cert service" << Log::Field(err);
+    }
+
+    err = PublicCurrentNodeService::Reconnect();
+    if (!err.IsNone()) {
+        LOG_ERR() << "Failed to reconnect public current node service" << Log::Field(err);
+    }
+}
+
+} // namespace aos::sm::iamclient
