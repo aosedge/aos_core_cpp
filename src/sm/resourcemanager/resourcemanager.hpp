cb232fdd1948fab52f3a03e712e08a4cfdcc11f9
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-12-04 18:32:59 +0200

    sm: resourcemanager: clear implementation
    This patch removes the existing implementation, as it
    became obsolete due to the new interface changes.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/sm/resourcemanager/resourcemanager.hpp
+++ src/sm/resourcemanager/resourcemanager.hpp
@@ -7,51 +7,9 @@
 #ifndef AOS_SM_RESOURCEMANAGER_RESOURCEMANAGER_HPP_
 #define AOS_SM_RESOURCEMANAGER_RESOURCEMANAGER_HPP_
 
-#include <set>
-#include <string>
-#include <vector>
-
-#include <core/sm/resourcemanager/resourcemanager.hpp>
+#include <core/sm/resourcemanager/itf/resourceinfoprovider.hpp>
 
 namespace aos::sm::resourcemanager {
-
-/**
- * Host device manager.
- */
-class HostDeviceManager : public HostDeviceManagerItf {
-public:
-    /**
-     * Initializes host device manager object.
-     *
-     * @return Error.
-     */
-    Error Init();
-
-    /**
-     * Checks if device exists.
-     *
-     * @param device device name.
-     * @return true if device exists, false otherwise.
-     */
-    Error CheckDevice(const String& device) const override;
-
-    /**
-     * Checks if group exists.
-     *
-     * @param group group name.
-     * @return true if group exists, false otherwise.
-     */
-    Error CheckGroup(const String& group) const override;
-
-private:
-    static constexpr auto cDevicesDirectory = "/dev/";
-    static constexpr auto cGroupsFile       = "/etc/group";
-
-    Error ParseGroups();
-
-    std::set<std::string> mDevices;
-    std::set<std::string> mGroups;
-};
 
 } // namespace aos::sm::resourcemanager
 
