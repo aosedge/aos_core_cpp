2bb9cb79f80b1159429a9d96e42d1f17abb5b6b3
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-12-09 19:26:08 +0200

    sm: utils: move systemdconn to utils module
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

---
+++ src/sm/tests/mocks/tests/mocks.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <sm/tests/mocks/journalmock.hpp>
+#include <sm/tests/mocks/systemdconnmock.hpp>
