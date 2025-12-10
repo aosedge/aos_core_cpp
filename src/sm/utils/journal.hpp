5cfdbf5fac034d0f319f624f26883eeb8679a773
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2025-12-10 10:47:50 +0200

    sm: utils: move interfaces to itf subfolder
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/sm/utils/journal.hpp
+++ src/sm/utils/journal.hpp
@@ -7,132 +7,9 @@
 #ifndef AOS_SM_UTILS_JOURNAL_HPP_
 #define AOS_SM_UTILS_JOURNAL_HPP_
 
-#include <core/common/tools/time.hpp>
-
-#include <optional>
-#include <string>
-
-/**
- * Forward declaration of systemd journal struct.
- */
-struct sd_journal;
+#include "itf/journal.hpp"
 
 namespace aos::sm::utils {
-
-/**
- * Journal entry.
- */
-struct JournalEntry {
-    /**
-     * Real time.
-     */
-    Time mRealTime;
-
-    /**
-     * Monotonic time.
-     */
-    Time mMonotonicTime;
-
-    /**
-     * Message.
-     */
-    std::string mMessage;
-
-    /**
-     * Systemd unit.
-     */
-    std::string mSystemdUnit;
-
-    /**
-     * Systemd cgroup.
-     */
-    std::string mSystemdCGroup;
-
-    /**
-     * Priority level.
-     */
-    int mPriority;
-
-    /**
-     * Optional "UNIT" field (produced by init.scope unit).
-     */
-    std::optional<std::string> mUnit;
-};
-
-/**
- * Interface for systemd journal.
- */
-class JournalItf {
-public:
-    /**
-     * Destructor.
-     */
-    virtual ~JournalItf() = default;
-
-    /**
-     * Seeks to a specific realtime timestamp.
-     *
-     * @param time realtime timestamp.
-     */
-    virtual void SeekRealtime(Time time) = 0;
-
-    /**
-     * Seeks to the tail of the journal.
-     */
-    virtual void SeekTail() = 0;
-
-    /**
-     * Seeks to the head of the journal.
-     */
-    virtual void SeekHead() = 0;
-
-    /**
-     * Adds a disjunction to the journal filter.
-     */
-    virtual void AddDisjunction() = 0;
-
-    /**
-     * Adds a match to the journal filter.
-     *
-     * @param match match.
-     */
-    virtual void AddMatch(const std::string& match) = 0;
-
-    /**
-     * Moves to the next journal entry.
-     *
-     * @return bool.
-     */
-    virtual bool Next() = 0;
-
-    /**
-     * Moves to the previous journal entry.
-     *
-     * @return bool.
-     */
-    virtual bool Previous() = 0;
-
-    /**
-     * Returns current journal entry.
-     *
-     * @return JournalEntry.
-     */
-    virtual JournalEntry GetEntry() = 0;
-
-    /**
-     * Seek to a specific cursor in the journal.
-     *
-     * @param cursor journal cursor.
-     */
-    virtual void SeekCursor(const std::string& cursor) = 0;
-
-    /**
-     * Get the current cursor position.
-     *
-     * @return std::string.
-     */
-    virtual std::string GetCursor() = 0;
-};
 
 /**
  * Journal.
@@ -214,7 +91,7 @@ public:
     std::string GetCursor() override;
 
 private:
-    sd_journal* mJournal = nullptr;
+    class sd_journal* mJournal {};
 };
 
 } // namespace aos::sm::utils
--- src/sm/utils/systemdconn.hpp
+++ src/sm/utils/systemdconn.hpp
@@ -8,96 +8,12 @@
 #define AOS_SM_UTILS_SYSTEMDCONN_HPP_
 
 #include <mutex>
-#include <string>
 #include <systemd/sd-bus.h>
 #include <vector>
 
-#include <core/common/tools/error.hpp>
-#include <core/common/tools/optional.hpp>
-#include <core/common/tools/time.hpp>
+#include "itf/systemdconn.hpp"
 
 namespace aos::sm::utils {
-
-/**
- * Unit state.
- */
-class UnitStateType {
-public:
-    enum class Enum { eActive, eInactive, eFailed, eActivating, eDeactivating, eMaintenance, eReloading, eRefreshing };
-
-    static const Array<const char* const> GetStrings()
-    {
-        static const char* const sNames[]
-            = {"active", "inactive", "failed", "activating", "deactivating", "maintenance", "reloading", "refreshing"};
-
-        return Array<const char* const>(sNames, ArraySize(sNames));
-    };
-};
-
-using UnitStateEnum = UnitStateType::Enum;
-using UnitState     = EnumStringer<UnitStateType>;
-
-/**
- * Unit status.
- */
-struct UnitStatus {
-    std::string       mName;
-    UnitState         mActiveState;
-    Optional<int32_t> mExitCode;
-};
-
-/**
- * Systemd dbus connection interface.
- */
-class SystemdConnItf {
-public:
-    /**
-     * Destructor.
-     */
-    virtual ~SystemdConnItf() = default;
-
-    /**
-     * Returns a list of systemd units.
-     *
-     * @return RetWithError<std::vector<UnitStatus>>.
-     */
-    virtual RetWithError<std::vector<UnitStatus>> ListUnits() = 0;
-
-    /**
-     * Returns a status of systemd unit.
-     *
-     * @param name unit name.
-     * @return RetWithError<UnitStatus>.
-     */
-    virtual RetWithError<UnitStatus> GetUnitStatus(const std::string& name) = 0;
-
-    /**
-     * Starts a unit.
-     *
-     * @param name unit name.
-     * @param mode start mode.
-     * @param timeout timeout.
-     * @return Error.
-     */
-    virtual Error StartUnit(const std::string& name, const std::string& mode, const aos::Duration& timeout) = 0;
-
-    /**
-     * Stops a unit.
-     *
-     * @param name unit name.
-     * @param mode start mode.
-     * @return Error.
-     */
-    virtual Error StopUnit(const std::string& name, const std::string& mode, const Duration& timeout) = 0;
-
-    /**
-     * Resets the "failed" state of a specific unit.
-     *
-     * @param name unit name.
-     * @return Error.
-     */
-    virtual Error ResetFailedUnit(const std::string& name) = 0;
-};
 
 /**
  * Systemd dbus connection.
