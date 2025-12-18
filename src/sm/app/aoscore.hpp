89af853c63b4243d2c96f06741b66ae5484814b3
Mykola Solianko <mykola_solianko@epam.com>
2025-12-18 13:28:12 +0200

    sm: aoscore: update sm initialization
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/sm/app/aoscore.hpp
+++ src/sm/app/aoscore.hpp
@@ -11,33 +11,26 @@
 
 #include <core/common/crypto/certloader.hpp>
 #include <core/common/crypto/cryptoprovider.hpp>
-#include <core/common/monitoring/resourcemonitor.hpp>
+#include <core/common/monitoring/monitoring.hpp>
 #include <core/sm/launcher/launcher.hpp>
-#include <core/sm/layermanager/layermanager.hpp>
-#include <core/sm/servicemanager/servicemanager.hpp>
+#include <core/sm/nodeconfig/nodeconfig.hpp>
 
-#include <common/downloader/downloader.hpp>
-#include <common/iamclient/permservicehandler.hpp>
-#include <common/iamclient/publicservicehandler.hpp>
+#include <common/iamclient/tlscredentials.hpp>
 #include <common/jsonprovider/jsonprovider.hpp>
 #include <common/logger/logger.hpp>
 #include <common/network/interfacemanager.hpp>
 #include <common/network/iptables.hpp>
 #include <common/network/namespacemanager.hpp>
-#include <common/ocispec/ocispec.hpp>
 #include <common/utils/cleanupmanager.hpp>
-#include <common/utils/fsplatform.hpp>
 #include <sm/alerts/journalalerts.hpp>
 #include <sm/database/database.hpp>
-#include <sm/image/imagehandler.hpp>
-#include <sm/launcher/runtime.hpp>
+#include <sm/iamclient/iamclient.hpp>
 #include <sm/logprovider/logprovider.hpp>
-#include <sm/monitoring/resourceusageprovider.hpp>
+#include <sm/monitoring/nodemonitoringprovider.hpp>
 #include <sm/networkmanager/cni.hpp>
 #include <sm/networkmanager/exec.hpp>
 #include <sm/networkmanager/trafficmonitor.hpp>
 #include <sm/resourcemanager/resourcemanager.hpp>
-#include <sm/runner/runner.hpp>
 #include <sm/smclient/smclient.hpp>
 
 namespace aos::sm::app {
@@ -77,43 +70,31 @@ public:
     void SetLogLevel(aos::LogLevel level);
 
 private:
-    config::Config                                       mConfig = {};
-    aos::crypto::CertLoader                              mCertLoader;
-    aos::crypto::DefaultCryptoProvider                   mCryptoProvider;
-    aos::monitoring::ResourceMonitor                     mResourceMonitor;
-    aos::pkcs11::PKCS11Manager                           mPKCS11Manager;
-    aos::common::utils::FSPlatform                       mPlatformFS;
-    aos::spaceallocator::SpaceAllocator<cMaxNumLayers>   mLayersSpaceAllocator;
-    aos::spaceallocator::SpaceAllocator<cMaxNumServices> mDownloadServicesSpaceAllocator;
-    aos::spaceallocator::SpaceAllocator<cMaxNumLayers>   mDownloadLayersSpaceAllocator;
-    aos::spaceallocator::SpaceAllocator<cMaxNumServices> mServicesSpaceAllocator;
-    common::downloader::Downloader                       mDownloader;
-    common::iamclient::PermissionsServiceHandler         mIAMClientPermissions;
-    common::iamclient::PublicServiceHandler              mIAMClientPublic;
-    common::jsonprovider::JSONProvider                   mJSONProvider;
-    common::logger::Logger                               mLogger;
-    common::oci::OCISpec                                 mOCISpec;
-    sm::cni::CNI                                         mCNI;
-    sm::cni::Exec                                        mExec;
-    sm::database::Database                               mDatabase;
-    sm::image::ImageHandler                              mImageHandler;
-    sm::launcher::Launcher                               mLauncher;
-    sm::launcher::Runtime                                mRuntime;
-    sm::layermanager::LayerManager                       mLayerManager;
-    sm::logprovider::LogProvider                         mLogProvider;
-    sm::monitoring::ResourceUsageProvider                mResourceUsageProvider;
-    sm::networkmanager::NetworkManager                   mNetworkManager;
-    sm::networkmanager::TrafficMonitor                   mTrafficMonitor;
-    common::network::IPTables                            mIPTables;
-    aos::common::network::NamespaceManager               mNamespaceManager;
-    aos::common::network::InterfaceManager               mNetworkInterfaceManager;
-    sm::resourcemanager::HostDeviceManager               mHostDeviceManager;
-    sm::resourcemanager::ResourceManager                 mResourceManager;
-    sm::runner::Runner                                   mRunner;
-    sm::servicemanager::ServiceManager                   mServiceManager;
-    sm::alerts::JournalAlerts                            mJournalAlerts;
-    sm::smclient::SMClient                               mSMClient;
-    aos::common::utils::CleanupManager                   mCleanupManager;
+    config::Config                         mConfig = {};
+    aos::crypto::CertLoader                mCertLoader;
+    aos::crypto::DefaultCryptoProvider     mCryptoProvider;
+    aos::monitoring::Monitoring            mMonitoring;
+    aos::pkcs11::PKCS11Manager             mPKCS11Manager;
+    common::iamclient::TLSCredentials      mTLSCredentials;
+    sm::iamclient::IAMClient               mIAMClient;
+    common::jsonprovider::JSONProvider     mJSONProvider;
+    common::logger::Logger                 mLogger;
+    sm::cni::CNI                           mCNI;
+    sm::cni::Exec                          mExec;
+    sm::database::Database                 mDatabase;
+    sm::launcher::Launcher                 mLauncher;
+    sm::logprovider::LogProvider           mLogProvider;
+    sm::monitoring::NodeMonitoringProvider mNodeMonitoringProvider;
+    sm::networkmanager::NetworkManager     mNetworkManager;
+    sm::networkmanager::TrafficMonitor     mTrafficMonitor;
+    common::network::IPTables              mIPTables;
+    aos::common::network::NamespaceManager mNamespaceManager;
+    aos::common::network::InterfaceManager mNetworkInterfaceManager;
+    sm::resourcemanager::ResourceManager   mResourceManager;
+    sm::nodeconfig::NodeConfig             mNodeConfigHandler;
+    // sm::alerts::JournalAlerts                            mJournalAlerts;
+    sm::smclient::SMClient             mSMClient;
+    aos::common::utils::CleanupManager mCleanupManager;
 
 private:
     static constexpr auto cDefaultConfigFile = "aos_servicemanager.cfg";
