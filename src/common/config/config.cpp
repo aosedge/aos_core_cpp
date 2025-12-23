3a8fd569567e7dbdaa0ccec21b720e433f2800df
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-12-23 19:08:26 +0200

    common: config: raise exception instead of returning errors
    Parse config functions returns errors but local config parsers raise exceptions.
    Unify error handling by using raising exceptions.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>

--- src/common/config/config.cpp
+++ src/common/config/config.cpp
@@ -28,64 +28,44 @@ constexpr auto cMinAlertPriorityLevel          = 0;
  * Public functions
  **********************************************************************************************************************/
 
-Error ParseMonitoringConfig(const common::utils::CaseInsensitiveObjectWrapper& object, monitoring::Config& config)
+void ParseMonitoringConfig(const common::utils::CaseInsensitiveObjectWrapper& object, monitoring::Config& config)
 {
-    try {
-        const auto pollPeriod    = object.GetValue<std::string>("pollPeriod", cDefaultMonitoringPollPeriod);
-        const auto averageWindow = object.GetValue<std::string>("averageWindow", cDefaultMonitoringAverageWindow);
+    const auto pollPeriod    = object.GetValue<std::string>("pollPeriod", cDefaultMonitoringPollPeriod);
+    const auto averageWindow = object.GetValue<std::string>("averageWindow", cDefaultMonitoringAverageWindow);
 
-        Error err = ErrorEnum::eNone;
+    Error err = ErrorEnum::eNone;
 
-        Tie(config.mPollPeriod, err) = common::utils::ParseDuration(pollPeriod);
-        if (err != ErrorEnum::eNone) {
-            return AOS_ERROR_WRAP(err);
-        }
+    Tie(config.mPollPeriod, err) = common::utils::ParseDuration(pollPeriod);
+    AOS_ERROR_CHECK_AND_THROW(err, "error parsing pollPeriod tag");
 
-        Tie(config.mAverageWindow, err) = common::utils::ParseDuration(averageWindow);
-
-        return AOS_ERROR_WRAP(err);
-    } catch (const std::exception& e) {
-        return common::utils::ToAosError(e);
-    }
+    Tie(config.mAverageWindow, err) = common::utils::ParseDuration(averageWindow);
+    AOS_ERROR_CHECK_AND_THROW(err, "error parsing averageWindow tag");
 }
 
-Error ParseMigrationConfig(const common::utils::CaseInsensitiveObjectWrapper& object,
+void ParseMigrationConfig(const common::utils::CaseInsensitiveObjectWrapper& object,
     const std::string& defaultMigrationPath, const std::string& defaultMergedMigrationPath, Migration& config)
 {
-    try {
-        config.mMigrationPath = object.GetOptionalValue<std::string>("migrationPath").value_or(defaultMigrationPath);
-        config.mMergedMigrationPath
-            = object.GetOptionalValue<std::string>("mergedMigrationPath").value_or(defaultMergedMigrationPath);
-    } catch (const std::exception& e) {
-        return common::utils::ToAosError(e);
-    }
-
-    return ErrorEnum::eNone;
+    config.mMigrationPath = object.GetOptionalValue<std::string>("migrationPath").value_or(defaultMigrationPath);
+    config.mMergedMigrationPath
+        = object.GetOptionalValue<std::string>("mergedMigrationPath").value_or(defaultMergedMigrationPath);
 }
 
-Error ParseJournalAlertsConfig(const common::utils::CaseInsensitiveObjectWrapper& object, JournalAlerts& config)
+void ParseJournalAlertsConfig(const common::utils::CaseInsensitiveObjectWrapper& object, JournalAlerts& config)
 {
-    try {
-        config.mFilter = common::utils::GetArrayValue<std::string>(object, "filter");
+    config.mFilter = common::utils::GetArrayValue<std::string>(object, "filter");
 
-        config.mServiceAlertPriority
-            = object.GetOptionalValue<int>("serviceAlertPriority").value_or(cDefaultServiceAlertPriority);
-        if (config.mServiceAlertPriority > cMaxAlertPriorityLevel
-            || config.mServiceAlertPriority < cMinAlertPriorityLevel) {
-            config.mServiceAlertPriority = cDefaultServiceAlertPriority;
-        }
-
-        config.mSystemAlertPriority
-            = object.GetOptionalValue<int>("systemAlertPriority").value_or(cDefaultSystemAlertPriority);
-        if (config.mSystemAlertPriority > cMaxAlertPriorityLevel
-            || config.mSystemAlertPriority < cMinAlertPriorityLevel) {
-            config.mSystemAlertPriority = cDefaultSystemAlertPriority;
-        }
-    } catch (const std::exception& e) {
-        return common::utils::ToAosError(e);
+    config.mServiceAlertPriority
+        = object.GetOptionalValue<int>("serviceAlertPriority").value_or(cDefaultServiceAlertPriority);
+    if (config.mServiceAlertPriority > cMaxAlertPriorityLevel
+        || config.mServiceAlertPriority < cMinAlertPriorityLevel) {
+        config.mServiceAlertPriority = cDefaultServiceAlertPriority;
     }
 
-    return ErrorEnum::eNone;
+    config.mSystemAlertPriority
+        = object.GetOptionalValue<int>("systemAlertPriority").value_or(cDefaultSystemAlertPriority);
+    if (config.mSystemAlertPriority > cMaxAlertPriorityLevel || config.mSystemAlertPriority < cMinAlertPriorityLevel) {
+        config.mSystemAlertPriority = cDefaultSystemAlertPriority;
+    }
 }
 
 } // namespace aos::common::config
