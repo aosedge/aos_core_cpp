1fa45c42fd12c644a0cd41f157b21f86837f85f7
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-12-23 19:05:27 +0200

    sm: launcher: add initial runtime skeletons and factory
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>

---
+++ src/sm/launcher/runtimes.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <core/common/tools/logger.hpp>
+
+#include "runtimes.hpp"
+#include "runtimes/boot/boot.hpp"
+#include "runtimes/container/container.hpp"
+#include "runtimes/rootfs/rootfs.hpp"
+
+namespace aos::sm::launcher {
+
+/***********************************************************************************************************************
+ * Public
+ **********************************************************************************************************************/
+
+Error Runtimes::Init(const Config& config, iamclient::CurrentNodeInfoProviderItf& currentNodeInfoProvider)
+{
+    LOG_DBG() << "Init runtimes" << Log::Field("numRuntimes", config.mRuntimes.size());
+
+    for (const auto& [name, runtimeConfig] : config.mRuntimes) {
+        LOG_DBG() << "Init runtime" << Log::Field("runtime", name.c_str())
+                  << Log::Field("type", runtimeConfig.mType.c_str());
+
+        if (name == cRuntimeContainer) {
+            auto runtime = std::make_unique<ContainerRuntime>();
+
+            if (auto err = runtime->Init(runtimeConfig, currentNodeInfoProvider); !err.IsNone()) {
+                return AOS_ERROR_WRAP(err);
+            }
+
+            mRuntimes.emplace_back(std::move(runtime));
+        } else if (name == cRuntimeBoot) {
+            auto runtime = std::make_unique<BootRuntime>();
+
+            if (auto err = runtime->Init(runtimeConfig, currentNodeInfoProvider); !err.IsNone()) {
+                return AOS_ERROR_WRAP(err);
+            }
+
+            mRuntimes.emplace_back(std::move(runtime));
+
+        } else if (name == cRuntimeRootfs) {
+            auto runtime = std::make_unique<RootfsRuntime>();
+
+            if (auto err = runtime->Init(runtimeConfig, currentNodeInfoProvider); !err.IsNone()) {
+                return AOS_ERROR_WRAP(err);
+            }
+
+            mRuntimes.emplace_back(std::move(runtime));
+        } else {
+            return AOS_ERROR_WRAP(Error(ErrorEnum::eNotSupported, "runtime is not supported"));
+        }
+    }
+
+    return ErrorEnum::eNone;
+}
+
+Error Runtimes::GetRuntimes(Array<RuntimeItf*>& runtimes) const
+{
+    for (const auto& runtime : mRuntimes) {
+        if (auto err = runtimes.EmplaceBack(runtime.get()); !err.IsNone()) {
+            return AOS_ERROR_WRAP(err);
+        }
+    }
+
+    return ErrorEnum::eNone;
+}
+
+} // namespace aos::sm::launcher
