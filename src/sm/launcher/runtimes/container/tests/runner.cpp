56be2b54628bbde03d44a05a5ad3c4a24a8b766e
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-12-27 17:30:52 +0200

    sm: luancher: container: update and refactor runner
    Use stl types and update to new interface.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/sm/launcher/runtimes/container/tests/runner.cpp
+++ src/sm/launcher/runtimes/container/tests/runner.cpp
@@ -11,28 +11,20 @@
 #include <core/common/tests/utils/log.hpp>
 
 #include <sm/launcher/runtimes/container/runner.hpp>
+#include <sm/tests/mocks/systemdconnmock.hpp>
 
-#include "mocks/runstatusreceivermock.hpp"
-#include "mocks/systemdconnmock.hpp"
+#include "mocks/runnermock.hpp"
 
 using namespace testing;
 
-namespace aos::sm::runner {
-
-namespace {
-
-std::filesystem::path GetRuntimeDir()
-{
-    const auto testDir = std::filesystem::canonical("/proc/self/exe").parent_path();
-
-    return testDir / "runtime";
-}
-
-} // namespace
+namespace aos::sm::launcher {
 
 class TestRunner : public Runner {
+public:
+    std::shared_ptr<utils::SystemdConnMock> mSystemd = std::make_shared<utils::SystemdConnMock>();
+
 private:
-    std::shared_ptr<SystemdConnItf> CreateSystemdConn() override { return mSystemd; }
+    std::shared_ptr<utils::SystemdConnItf> CreateSystemdConn() override { return mSystemd; }
 
     std::string GetSystemdDropInsDir() const override
     {
@@ -40,47 +32,36 @@ private:
 
         return testDir / "systemd";
     }
-
-    std::shared_ptr<SystemdConnMock> mSystemd = std::make_shared<SystemdConnMock>();
 };
 
-class RunnerTest : public Test {
+class ContainerRunnerTest : public Test {
 public:
-    void SetUp() override
-    {
-        tests::utils::InitLog();
+    static void SetUpTestSuite() { tests::utils::InitLog(); }
 
-        mRunner.Init(mRunStatusReceiver);
-    }
+    void SetUp() override { mRunner.Init(mRunStatusReceiver); }
 
 protected:
-    const std::filesystem::path cRuntimeDir = GetRuntimeDir();
-
     RunStatusReceiverMock mRunStatusReceiver;
-
-    TestRunner mRunner;
+    TestRunner            mRunner;
 };
 
 /***********************************************************************************************************************
  * Tests
  **********************************************************************************************************************/
 
-TEST_F(RunnerTest, StartInstance)
+TEST_F(ContainerRunnerTest, StartInstance)
 {
-    RunParameters params = {{500 * Time::cMilliseconds}, {0}, {0}};
-    UnitStatus    status = {"aos-service@service0.service", UnitStateEnum::eActive, 0};
-    Error         err    = ErrorEnum::eNone;
+    RunParameters     params = {{500 * Time::cMilliseconds}, {0}, {0}};
+    utils::UnitStatus status = {"aos-service@service0.service", utils::UnitStateEnum::eActive, 0};
+    Error             err    = ErrorEnum::eNone;
 
     EXPECT_CALL(*mRunner.mSystemd, StartUnit("aos-service@service0.service", "replace", _)).WillOnce(Return(err));
-    EXPECT_CALL(*mRunner.mSystemd, GetUnitStatus(_)).WillOnce(Return(RetWithError<UnitStatus>(status, err)));
+    EXPECT_CALL(*mRunner.mSystemd, GetUnitStatus(_)).WillOnce(Return(RetWithError<utils::UnitStatus>(status, err)));
 
-    std::vector<UnitStatus> units = {status};
+    std::vector<utils::UnitStatus> units = {status};
     EXPECT_CALL(*mRunner.mSystemd, ListUnits())
-        .WillRepeatedly(Return(RetWithError<std::vector<UnitStatus>>(units, err)));
-
-    StaticArray<RunStatus, 1> expectedInstances;
-
-    expectedInstances.PushBack(RunStatus {"service0", InstanceStateEnum::eActive, Error()});
+        .WillRepeatedly(Return(RetWithError<std::vector<utils::UnitStatus>>(units, err)));
+    std::vector<RunStatus> expectedInstances {{"service0", InstanceStateEnum::eActive, Error()}};
 
     EXPECT_CALL(mRunStatusReceiver, UpdateRunStatus(expectedInstances)).Times(1);
 
@@ -88,7 +69,7 @@ TEST_F(RunnerTest, StartInstance)
 
     const auto expectedRes = RunStatus {"service0", InstanceStateEnum::eActive, ErrorEnum::eNone};
 
-    EXPECT_EQ(mRunner.StartInstance("service0", cRuntimeDir.c_str(), params), expectedRes);
+    EXPECT_EQ(mRunner.StartInstance("service0", params), expectedRes);
 
     sleep(2); // wait to monitor
 
@@ -100,7 +81,7 @@ TEST_F(RunnerTest, StartInstance)
     mRunner.Stop();
 }
 
-TEST_F(RunnerTest, StartUnitFailed)
+TEST_F(ContainerRunnerTest, StartUnitFailed)
 {
     RunParameters params = {};
 
@@ -111,31 +92,31 @@ TEST_F(RunnerTest, StartUnitFailed)
 
     const auto expectedRes = RunStatus {"service0", InstanceStateEnum::eFailed, ErrorEnum::eFailed};
 
-    EXPECT_EQ(mRunner.StartInstance("service0", cRuntimeDir.c_str(), params), expectedRes);
+    EXPECT_EQ(mRunner.StartInstance("service0", params), expectedRes);
 
     mRunner.Stop();
 }
 
-TEST_F(RunnerTest, GetUnitStatusFailed)
+TEST_F(ContainerRunnerTest, GetUnitStatusFailed)
 {
     mRunner.Start();
 
-    RunParameters params = {};
-    UnitStatus    status = {"aos-service@service0.service", UnitStateEnum::eFailed, 1};
-    Error         err    = ErrorEnum::eFailed;
+    RunParameters     params = {};
+    utils::UnitStatus status = {"aos-service@service0.service", utils::UnitStateEnum::eFailed, 1};
+    Error             err    = ErrorEnum::eFailed;
 
     EXPECT_CALL(*mRunner.mSystemd, StartUnit("aos-service@service0.service", "replace", _)).WillOnce(Return(Error()));
     EXPECT_CALL(*mRunner.mSystemd, GetUnitStatus("aos-service@service0.service"))
-        .WillOnce(Return(RetWithError<UnitStatus>(status, err)));
+        .WillOnce(Return(RetWithError<utils::UnitStatus>(status, err)));
 
     const auto expectedRes = RunStatus {"service0", InstanceStateEnum::eFailed, ErrorEnum::eFailed};
 
-    EXPECT_EQ(mRunner.StartInstance("service0", cRuntimeDir.c_str(), params), expectedRes);
+    EXPECT_EQ(mRunner.StartInstance("service0", params), expectedRes);
 
     mRunner.Stop();
 }
 
-TEST_F(RunnerTest, ListUnitsFailed)
+TEST_F(ContainerRunnerTest, ListUnitsFailed)
 {
     mRunner.Start();
 
@@ -146,16 +127,16 @@ TEST_F(RunnerTest, ListUnitsFailed)
 
     const auto expectedRes = RunStatus {"service0", InstanceStateEnum::eFailed, ErrorEnum::eFailed};
 
-    EXPECT_EQ(mRunner.StartInstance("service0", cRuntimeDir.c_str(), params), expectedRes);
+    EXPECT_EQ(mRunner.StartInstance("service0", params), expectedRes);
 
-    UnitStatus              status = {"aos-service@service0.service", UnitStateEnum::eFailed, 1};
-    std::vector<UnitStatus> units  = {status};
+    utils::UnitStatus              status = {"aos-service@service0.service", utils::UnitStateEnum::eFailed, 1};
+    std::vector<utils::UnitStatus> units  = {status};
 
     EXPECT_CALL(*mRunner.mSystemd, ListUnits())
-        .WillOnce(Return(RetWithError<std::vector<UnitStatus>>(units, Error(ErrorEnum::eFailed))));
+        .WillOnce(Return(RetWithError<std::vector<utils::UnitStatus>>(units, Error(ErrorEnum::eFailed))));
     sleep(2); // wait to monitor
 
     mRunner.Stop();
 }
 
-} // namespace aos::sm::runner
+} // namespace aos::sm::launcher
