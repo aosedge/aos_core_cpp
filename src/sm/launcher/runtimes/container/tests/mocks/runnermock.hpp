e31912dad373b3a651570eeb00da71b4a4961110
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-12-27 17:30:52 +0200

    sm: luancher: container: update and refactor runner
    Use stl types and update to new interface.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

---
+++ src/sm/launcher/runtimes/container/tests/mocks/runnermock.hpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_TESTS_MOCKS_RUNNERMOCK_HPP_
+#define AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_TESTS_MOCKS_RUNNERMOCK_HPP_
+
+#include <gmock/gmock.h>
+
+#include <sm/launcher/runtimes/container/itf/runner.hpp>
+
+namespace aos::sm::launcher {
+
+/**
+ * Run status receiver mock.
+ */
+class RunStatusReceiverMock : public RunStatusReceiverItf {
+public:
+    MOCK_METHOD(Error, UpdateRunStatus, (const std::vector<RunStatus>&), (override));
+};
+
+} // namespace aos::sm::launcher
+
+#endif
