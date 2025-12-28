b0601371a5cda4b09e874d6dd5cda354af38c630
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-12-28 19:40:33 +0200

    sm: launcher: container: add file system class implementation
    This class implements high level file system operations required to launch
    containers.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

---
+++ src/sm/launcher/runtimes/container/itf/filesystem.hpp
@@ -0,0 +1,133 @@
+/*
+ * Copyright (C) 2025 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_ITF_FILESYSTEM_HPP_
+#define AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_ITF_FILESYSTEM_HPP_
+
+#include <string>
+#include <vector>
+
+#include <core/common/ocispec/itf/ocispec.hpp>
+
+namespace aos::sm::launcher {
+
+/**
+ * File system interface.
+ */
+class FileSystemItf {
+public:
+    /**
+     * Destroys runtime interface.
+     */
+    virtual ~FileSystemItf() = default;
+
+    /**
+     * Creates host FS whiteouts.
+     *
+     * @param path path to whiteouts.
+     * @param hostBinds host binds.
+     * @return Error.
+     */
+    virtual Error CreateHostFSWhiteouts(const std::string& path, const std::vector<std::string>& hostBinds) = 0;
+
+    /**
+     * Creates mount points.
+     *
+     * @param mountPointDir mount point directory.
+     * @param mounts mounts to create.
+     * @return Error.
+     */
+    virtual Error CreateMountPoints(const std::string& mountPointDir, const std::vector<Mount>& mounts) = 0;
+
+    /**
+     * Mounts root FS for Aos service.
+     *
+     * @param rootfsPath path to service root FS.
+     * @param layers layers to mount.
+     * @return Error.
+     */
+    virtual Error MountServiceRootFS(const std::string& rootfsPath, const std::vector<std::string>& layers) = 0;
+
+    /**
+     * Umounts Aos service root FS.
+     *
+     * @param rootfsPath path to service root FS.
+     * @return Error.
+     */
+    virtual Error UmountServiceRootFS(const std::string& rootfsPath) = 0;
+
+    /**
+     * Prepares Aos service storage directory.
+     *
+     * @param path service storage directory.
+     * @param uid user ID.
+     * @param gid group ID.
+     * @return Error.
+     */
+    virtual Error PrepareServiceStorage(const std::string& path, uid_t uid, gid_t gid) = 0;
+
+    /**
+     * Prepares Aos service state file.
+     *
+     * @param path service state file path.
+     * @param uid user ID.
+     * @param gid group ID.
+     * @return Error.
+     */
+    virtual Error PrepareServiceState(const std::string& path, uid_t uid, gid_t gid) = 0;
+
+    /**
+     * Prepares directory for network files.
+     *
+     * @param path network directory path.
+     * @return Error.
+     */
+    virtual Error PrepareNetworkDir(const std::string& path) = 0;
+
+    /**
+     * Returns absolute path of FS item.
+     *
+     * @param path path to convert.
+     * @return RetWithError<std::string>.
+     */
+    virtual RetWithError<std::string> GetAbsPath(const std::string& path) = 0;
+
+    /**
+     * Returns GID by group name.
+     *
+     * @param groupName group name.
+     * @return RetWithError<gid_t>.
+     */
+    virtual RetWithError<gid_t> GetGIDByName(const std::string& groupName) = 0;
+
+    /**
+     * Populates host devices.
+     *
+     * @param devicePath device path.
+     * @param[out] devices OCI devices.
+     */
+    virtual Error PopulateHostDevices(const std::string& devicePath, std::vector<oci::LinuxDevice>& devices) = 0;
+
+    /**
+     * Clears directory.
+     *
+     * @param path directory path.
+     * @return Error.
+     */
+    virtual Error ClearDir(const std::string& path) = 0;
+
+    /**
+     * Removes all files and directories at path.
+     *
+     * @param path path to remove.
+     * @return Error.
+     */
+    virtual Error RemoveAll(const std::string& path) = 0;
+};
+
+} // namespace aos::sm::launcher
+
+#endif
---
+++ src/sm/launcher/runtimes/container/tests/filesystem.cpp
@@ -0,0 +1,236 @@
+/*
+ * Copyright (C) 2024 EPAM Systems, Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <filesystem>
+#include <fstream>
+#include <grp.h>
+#include <sys/stat.h>
+
+#include <gtest/gtest.h>
+
+#include <core/common/tests/utils/log.hpp>
+#include <core/common/tests/utils/utils.hpp>
+
+#include <sm/launcher/runtimes/container/filesystem.hpp>
+
+using namespace testing;
+
+namespace aos::sm::launcher {
+
+namespace fs = std::filesystem;
+
+namespace {
+
+/***********************************************************************************************************************
+ * Consts
+ **********************************************************************************************************************/
+
+constexpr auto cTestDirRoot = "/tmp/test_dir/launcher";
+
+/***********************************************************************************************************************
+ * Static
+ **********************************************************************************************************************/
+
+void CreateFile(const fs::path& filePath, const std::string& payload)
+{
+    std::ofstream file(filePath);
+
+    file << payload;
+    file.close();
+}
+
+} // namespace
+
+/***********************************************************************************************************************
+ * Suite
+ **********************************************************************************************************************/
+
+class ContainerFileSystemTest : public Test {
+protected:
+    static void SetUpTestSuite() { tests::utils::InitLog(); }
+
+    void SetUp() override { fs::remove_all(cTestDirRoot); }
+
+    void TearDown() override { fs::remove_all(cTestDirRoot); }
+
+    FileSystem mFileSystem;
+};
+
+/***********************************************************************************************************************
+ * Tests
+ **********************************************************************************************************************/
+
+TEST_F(ContainerFileSystemTest, CreateHostFSWhiteouts)
+{
+    std::vector<std::string> hostBinds     = {"bin", "sbin", "lib", "lib64", "usr"};
+    auto                     whiteoutsPath = fs::path(cTestDirRoot) / "host" / "whiteouts";
+
+    auto err = mFileSystem.CreateHostFSWhiteouts(whiteoutsPath.string(), hostBinds);
+    EXPECT_EQ(err, ErrorEnum::eNone) << "CreateHostFSWhiteouts failed: " << tests::utils::ErrorToStr(err);
+
+    for (const auto& entry : fs::directory_iterator(whiteoutsPath)) {
+        auto item = entry.path().filename();
+
+        EXPECT_TRUE(fs::exists(fs::path("/") / item));
+
+        auto status = fs::status(entry.path());
+
+        EXPECT_TRUE(fs::is_character_file(status));
+        EXPECT_EQ(status.permissions(), fs::perms::none);
+
+        EXPECT_EQ(std::find(hostBinds.begin(), hostBinds.end(), item.string()), hostBinds.end());
+    }
+}
+
+TEST_F(ContainerFileSystemTest, CreateMountPoints)
+{
+    std::vector<Mount> mounts = {
+        Mount {"proc", "proc", "proc"},
+        Mount {"tmpfs", "tmpfs", "tmpfs"},
+        Mount {"sysfs", "sysfs", "sysfs"},
+        Mount {"/etc/hosts", "etc/hosts", "bind", "rbind,ro"},
+        Mount {"/var/log", "var/log", "bind", "rbind"},
+        Mount {"/tmp", "tmp", "bind", "rw"},
+    };
+
+    auto mountPointDir = fs::path(cTestDirRoot) / "mountpoints";
+
+    auto err = mFileSystem.CreateMountPoints(mountPointDir.string(), mounts);
+    EXPECT_EQ(err, ErrorEnum::eNone) << "CreateMountPoints failed: " << tests::utils::ErrorToStr(err);
+
+    for (const auto& mount : mounts) {
+        auto mountPoint = mountPointDir / mount.mDestination.CStr();
+
+        EXPECT_TRUE(fs::exists(mountPoint)) << "Mount point not created: " << mountPoint.string();
+
+        if (mount.mType == "proc" || mount.mType == "tmpfs" || mount.mType == "sysfs") {
+            EXPECT_TRUE(fs::is_directory(mountPoint));
+        } else {
+            if (fs::is_directory(mount.mSource.CStr())) {
+                EXPECT_TRUE(fs::is_directory(mountPoint));
+            } else {
+                EXPECT_TRUE(fs::is_regular_file(mountPoint));
+            }
+        }
+    }
+}
+
+TEST_F(ContainerFileSystemTest, PrepareNetworkDir)
+{
+    auto networkDir = fs::path(cTestDirRoot) / "network";
+
+    auto err = mFileSystem.PrepareNetworkDir(networkDir.string());
+    EXPECT_EQ(err, ErrorEnum::eNone) << "PrepareNetworkDir failed: " << tests::utils::ErrorToStr(err);
+
+    EXPECT_TRUE(fs::exists(networkDir / "etc")) << "Network etc dir not created";
+}
+
+TEST_F(ContainerFileSystemTest, GetAbsPath)
+{
+    auto relativePath = fs::path("some") / "relative" / "path";
+    auto expectedPath = fs::absolute(relativePath).string();
+
+    auto [absPath, err] = mFileSystem.GetAbsPath(relativePath.string());
+    EXPECT_EQ(err, ErrorEnum::eNone) << "GetAbsPath failed: " << tests::utils::ErrorToStr(err);
+
+    EXPECT_EQ(absPath, expectedPath) << "Absolute path mismatch";
+}
+
+TEST_F(ContainerFileSystemTest, GetGIDByName)
+{
+    const std::string groupName = "nogroup";
+
+    auto [gid, err] = mFileSystem.GetGIDByName(groupName);
+    EXPECT_EQ(err, ErrorEnum::eNone) << "GetGIDByName failed: " << tests::utils::ErrorToStr(err);
+
+    auto grp = getgrnam(groupName.c_str());
+    EXPECT_NE(grp, nullptr) << "can't get group id";
+
+    EXPECT_EQ(gid, grp->gr_gid) << "GID mismatch";
+}
+
+TEST_F(ContainerFileSystemTest, PopulateHostDevices)
+{
+    const auto cRootDevicePath     = fs::path(cTestDirRoot) / "dev";
+    const auto cTestDeviceFullPath = cRootDevicePath / "device1";
+
+    if (!fs::exists(cRootDevicePath)) {
+        fs::create_directories(cRootDevicePath);
+    }
+
+    if (auto res = mknod(cTestDeviceFullPath.c_str(), S_IFCHR, 0); res != 0) {
+        FAIL() << "Can't create test device node: " << strerror(errno);
+    }
+
+    std::vector<oci::LinuxDevice> devices;
+
+    auto err = mFileSystem.PopulateHostDevices(cTestDeviceFullPath, devices);
+    EXPECT_EQ(err, ErrorEnum::eNone) << "PopulateHostDevices failed: " << tests::utils::ErrorToStr(err);
+
+    EXPECT_EQ(devices.size(), 1);
+    EXPECT_EQ(devices.front().mPath, cTestDeviceFullPath.c_str());
+}
+
+TEST_F(ContainerFileSystemTest, PopulateHostDevicesSymlink)
+{
+    const auto cRootDevicePath     = fs::path(cTestDirRoot) / "dev";
+    const auto cTestDeviceFullPath = cRootDevicePath / "device1";
+
+    if (!fs::exists(cRootDevicePath)) {
+        fs::create_directories(cRootDevicePath);
+    }
+
+    if (auto res = mknod(cTestDeviceFullPath.c_str(), S_IFCHR, 0); res != 0) {
+        FAIL() << "Can't create test device node: " << strerror(errno);
+    }
+
+    const auto currentPath = fs::current_path();
+
+    fs::current_path(cRootDevicePath);
+    fs::create_symlink("device1", "link");
+    fs::current_path(currentPath);
+
+    std::vector<oci::LinuxDevice> devices;
+
+    auto err = mFileSystem.PopulateHostDevices((cRootDevicePath / "link"), devices);
+    EXPECT_EQ(err, ErrorEnum::eNone) << "PopulateHostDevices failed: " << tests::utils::ErrorToStr(err);
+
+    EXPECT_EQ(devices.size(), 1);
+    EXPECT_EQ(devices.front().mPath, (cRootDevicePath / "link").c_str());
+}
+
+TEST_F(ContainerFileSystemTest, ClearDir)
+{
+    const auto testDir = fs::path(cTestDirRoot) / "dir";
+
+    fs::create_directories(testDir / "subdir");
+
+    CreateFile(testDir / "file1", "test");
+    CreateFile(testDir / "subdir" / "file2", "test");
+
+    auto err = mFileSystem.ClearDir(testDir.string());
+    EXPECT_EQ(err, ErrorEnum::eNone) << "ClearDir failed: " << tests::utils::ErrorToStr(err);
+
+    EXPECT_TRUE(fs::exists(testDir)) << "Directory removed";
+    EXPECT_TRUE(fs::is_empty(testDir)) << "Directory not empty";
+}
+
+TEST_F(ContainerFileSystemTest, RemoveAll)
+{
+    const auto testDir = fs::path(cTestDirRoot) / "dir";
+
+    fs::create_directories(testDir / "subdir");
+
+    CreateFile(testDir / "file1", "test");
+    CreateFile(testDir / "subdir" / "file2", "test");
+
+    auto err = mFileSystem.RemoveAll(testDir.string());
+    EXPECT_EQ(err, ErrorEnum::eNone) << "RemoveAll failed: " << tests::utils::ErrorToStr(err);
+
+    EXPECT_FALSE(fs::exists(testDir)) << "Directory not removed";
+}
+
+} // namespace aos::sm::launcher
