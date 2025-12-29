11df5ba2a53b5a4a656f35e45b42523a2d55f4c3
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2025-12-29 13:54:07 +0200

    sm: launcher: container: add initial instance class and unit test
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/sm/launcher/runtimes/container/container.hpp
+++ src/sm/launcher/runtimes/container/container.hpp
@@ -7,10 +7,20 @@
 #ifndef AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_CONTAINER_HPP_
 #define AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_CONTAINER_HPP_
 
+#include <condition_variable>
+#include <mutex>
+#include <unordered_map>
+
 #include <core/common/iamclient/itf/currentnodeinfoprovider.hpp>
 #include <core/sm/launcher/itf/runtime.hpp>
 
+#include <common/utils/utils.hpp>
 #include <sm/launcher/runtimes/config.hpp>
+
+#include "itf/filesystem.hpp"
+#include "itf/runner.hpp"
+
+#include "instance.hpp"
 
 namespace aos::sm::launcher {
 
@@ -22,7 +32,7 @@ constexpr auto cRuntimeContainer = "container";
 /**
  * Container runtime implementation.
  */
-class ContainerRuntime : public RuntimeItf {
+class ContainerRuntime : public RuntimeItf, public RunStatusReceiverItf {
 public:
     /**
      * Initializes container runtime.
@@ -58,20 +68,20 @@ public:
     /**
      * Start instance.
      *
-     * @param instance instance to start.
+     * @param instanceInfo instance to start.
      * @param[out] status instance status.
      * @return Error.
      */
-    Error StartInstance(const InstanceInfo& instance, InstanceStatus& status) override;
+    Error StartInstance(const InstanceInfo& instanceInfo, InstanceStatus& status) override;
 
     /**
      * Stop instance.
      *
-     * @param instance instance to stop.
+     * @param instanceIdent instance to stop.
      * @param[out] status instance status.
      * @return Error.
      */
-    Error StopInstance(const InstanceIdent& instance, InstanceStatus& status) override;
+    Error StopInstance(const InstanceIdent& instanceIdent, InstanceStatus& status) override;
 
     /**
      * Reboots runtime.
@@ -91,9 +101,20 @@ public:
         const InstanceIdent& instanceIdent, monitoring::InstanceMonitoringData& monitoringData) override;
 
 private:
+    virtual std::shared_ptr<RunnerItf>     CreateRunner();
+    virtual std::shared_ptr<FileSystemItf> CreateFileSystem();
+
+    Error UpdateRunStatus(const std::vector<RunStatus>& instances) override;
+
     Error CreateRuntimeInfo(const std::string& runtimeType, const NodeInfo& nodeInfo);
 
-    RuntimeInfo mRuntimeInfo;
+    std::shared_ptr<RunnerItf>     mRunner;
+    std::shared_ptr<FileSystemItf> mFileSystem;
+
+    RuntimeInfo                                                  mRuntimeInfo;
+    std::mutex                                                   mMutex;
+    std::condition_variable                                      mCV;
+    std::unordered_map<InstanceIdent, std::shared_ptr<Instance>> mCurrentInstances;
 };
 
 } // namespace aos::sm::launcher
