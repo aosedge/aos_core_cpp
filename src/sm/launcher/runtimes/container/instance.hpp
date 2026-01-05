731f20b0fbb8be18e542423285bbd9d69e6e4a87
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2026-01-05 12:43:44 +0200

    sm: launcher: container: add loading and creating OCI configs
    Add loading image and service configs and creating runtime config.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/sm/launcher/runtimes/container/instance.hpp
+++ src/sm/launcher/runtimes/container/instance.hpp
@@ -7,7 +7,14 @@
 #ifndef AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_INSTANCE_HPP_
 #define AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_INSTANCE_HPP_
 
+#include <mutex>
+
+#include <core/common/ocispec/itf/ocispec.hpp>
 #include <core/common/types/instance.hpp>
+#include <core/sm/imagemanager/itf/iteminfoprovider.hpp>
+
+#include "itf/filesystem.hpp"
+#include "itf/runner.hpp"
 
 #include "config.hpp"
 
@@ -22,8 +29,14 @@ public:
      * Constructor.
      *
      * @param instanceInfo instance info.
+     * @param config container runtime config.
+     * @param fileSystem file system interface.
+     * @param runner runner interface.
+     * @param itemInfoProvider item info provider.
+     * @param ociSpec OCI spec interface.
      */
-    Instance(const InstanceInfo& instance);
+    Instance(const InstanceInfo& instance, const ContainerConfig& config, FileSystemItf& fileSystem, RunnerItf& runner,
+        imagemanager::ItemInfoProviderItf& itemInfoProvider, oci::OCISpecItf& ociSpec);
 
     /**
      * Starts instance.
@@ -54,10 +67,24 @@ public:
     void GetStatus(InstanceStatus& status) const;
 
 private:
-    void GenerateInstanceID();
+    static constexpr auto cRuntimeConfigFile = "config.json";
+
+    void  GenerateInstanceID();
+    Error LoadConfigs(oci::ImageConfig& imageConfig, oci::ServiceConfig& serviceConfig);
+    Error CreateRuntimeConfig(const std::string& runtimeDir, const oci::ImageConfig& imageConfig,
+        const oci::ServiceConfig& serviceConfig, oci::RuntimeConfig& runtimeConfig);
 
     InstanceInfo mInstanceInfo;
     std::string  mInstanceID;
+    RunStatus    mRunStatus;
+
+    const ContainerConfig&             mConfig;
+    FileSystemItf&                     mFileSystem;
+    RunnerItf&                         mRunner;
+    imagemanager::ItemInfoProviderItf& mItemInfoProvider;
+    oci::OCISpecItf&                   mOCISpec;
+
+    mutable std::mutex mMutex;
 };
 
 } // namespace aos::sm::launcher
