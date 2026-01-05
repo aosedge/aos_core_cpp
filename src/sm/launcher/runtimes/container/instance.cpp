e86377d29d4308e06bb98c41c88d6d4daa1f237f
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2026-01-05 12:43:44 +0200

    sm: launcher: container: add loading and creating OCI configs
    Add loading image and service configs and creating runtime config.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/sm/launcher/runtimes/container/instance.cpp
+++ src/sm/launcher/runtimes/container/instance.cpp
@@ -4,8 +4,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <filesystem>
+
 #include <core/common/tools/logger.hpp>
 
+#include <common/utils/filesystem.hpp>
 #include <common/utils/utils.hpp>
 
 #include "instance.hpp"
@@ -16,8 +19,14 @@ namespace aos::sm::launcher {
  * Public
  **********************************************************************************************************************/
 
-Instance::Instance(const InstanceInfo& instance)
+Instance::Instance(const InstanceInfo& instance, const ContainerConfig& config, FileSystemItf& fileSystem,
+    RunnerItf& runner, imagemanager::ItemInfoProviderItf& itemInfoProvider, oci::OCISpecItf& ociSpec)
     : mInstanceInfo(instance)
+    , mConfig(config)
+    , mFileSystem(fileSystem)
+    , mRunner(runner)
+    , mItemInfoProvider(itemInfoProvider)
+    , mOCISpec(ociSpec)
 {
     GenerateInstanceID();
 
@@ -27,22 +36,68 @@ Instance::Instance(const InstanceInfo& instance)
 
 Error Instance::Start()
 {
+    std::lock_guard lock {mMutex};
+
+    auto runtimeDir = common::utils::JoinPath(mConfig.mRuntimeDir, mInstanceID);
+
+    if (auto err = mFileSystem.ClearDir(runtimeDir); !err.IsNone()) {
+        return AOS_ERROR_WRAP(err);
+    }
+
+    auto imageConfig   = std::make_unique<oci::ImageConfig>();
+    auto serviceConfig = std::make_unique<oci::ServiceConfig>();
+    auto runtimeConfig = std::make_unique<oci::RuntimeConfig>();
+
+    if (auto err = LoadConfigs(*imageConfig, *serviceConfig); !err.IsNone()) {
+        return err;
+    }
+
+    if (auto err = CreateRuntimeConfig(runtimeDir, *imageConfig, *serviceConfig, *runtimeConfig); !err.IsNone()) {
+        return err;
+    }
+
+    mRunStatus = mRunner.StartInstance(mInstanceID, serviceConfig->mRunParameters);
+
+    if (!mRunStatus.mError.IsNone()) {
+        return AOS_ERROR_WRAP(mRunStatus.mError);
+    }
+
     return ErrorEnum::eNone;
 }
 
 Error Instance::Stop()
 {
-    return ErrorEnum::eNone;
+    std::lock_guard lock {mMutex};
+
+    Error stopErr;
+
+    auto runtimeDir = common::utils::JoinPath(mConfig.mRuntimeDir, mInstanceID);
+
+    if (auto err = mRunner.StopInstance(mInstanceID); !err.IsNone() && stopErr.IsNone()) {
+        stopErr = AOS_ERROR_WRAP(err);
+    }
+
+    if (auto err = mFileSystem.RemoveAll(runtimeDir); !err.IsNone()) {
+        stopErr = AOS_ERROR_WRAP(err);
+    }
+
+    mRunStatus.mInstanceID = mInstanceID;
+    mRunStatus.mState      = InstanceStateEnum::eInactive;
+    mRunStatus.mError      = stopErr;
+
+    return stopErr;
 }
 
 void Instance::GetStatus(InstanceStatus& status) const
 {
+    std::lock_guard lock {mMutex};
+
     static_cast<InstanceIdent&>(status) = static_cast<const InstanceIdent&>(mInstanceInfo);
     status.mPreinstalled                = false;
     status.mRuntimeID                   = mInstanceInfo.mRuntimeID;
     status.mManifestDigest              = mInstanceInfo.mManifestDigest;
-    status.mState                       = InstanceStateEnum::eActivating;
-    status.mError                       = ErrorEnum::eNone;
+    status.mState                       = mRunStatus.mState;
+    status.mError                       = mRunStatus.mError;
 }
 
 /***********************************************************************************************************************
@@ -55,6 +110,62 @@ void Instance::GenerateInstanceID()
         + std::to_string(mInstanceInfo.mInstance);
 
     mInstanceID = common::utils::NameUUID(idStr);
+}
+
+Error Instance::LoadConfigs(oci::ImageConfig& imageConfig, oci::ServiceConfig& serviceConfig)
+{
+    auto path = std::make_unique<StaticString<cFilePathLen>>();
+
+    if (auto err = mItemInfoProvider.GetBlobPath(mInstanceInfo.mManifestDigest, *path); !err.IsNone()) {
+        return AOS_ERROR_WRAP(err);
+    }
+
+    auto manifest = std::make_unique<oci::ImageManifest>();
+
+    if (auto err = mOCISpec.LoadImageManifest(*path, *manifest); !err.IsNone()) {
+        return AOS_ERROR_WRAP(err);
+    }
+
+    if (auto err = mItemInfoProvider.GetBlobPath(manifest->mConfig.mDigest, *path); !err.IsNone()) {
+        return AOS_ERROR_WRAP(err);
+    }
+
+    if (auto err = mOCISpec.LoadImageConfig(*path, imageConfig); !err.IsNone()) {
+        return AOS_ERROR_WRAP(err);
+    }
+
+    if (manifest->mAosService.HasValue()) {
+        if (auto err = mItemInfoProvider.GetBlobPath(manifest->mAosService->mDigest, *path); !err.IsNone()) {
+            return AOS_ERROR_WRAP(err);
+        }
+
+        if (auto err = mOCISpec.LoadServiceConfig(*path, serviceConfig); !err.IsNone()) {
+            return AOS_ERROR_WRAP(err);
+        }
+    }
+
+    return ErrorEnum::eNone;
+}
+
+Error Instance::CreateRuntimeConfig(const std::string& runtimeDir, const oci::ImageConfig& imageConfig,
+    const oci::ServiceConfig& serviceConfig, oci::RuntimeConfig& runtimeConfig)
+{
+    (void)imageConfig;
+    (void)serviceConfig;
+
+    LOG_DBG() << "Create runtime config" << Log::Field("instanceID", mInstanceID.c_str());
+
+    if (auto err = oci::CreateExampleRuntimeConfig(runtimeConfig); !err.IsNone()) {
+        return AOS_ERROR_WRAP(err);
+    }
+
+    if (auto err
+        = mOCISpec.SaveRuntimeConfig(common::utils::JoinPath(runtimeDir, cRuntimeConfigFile).c_str(), runtimeConfig);
+        !err.IsNone()) {
+        return err;
+    }
+
+    return ErrorEnum::eNone;
 }
 
 } // namespace aos::sm::launcher
