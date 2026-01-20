0bdd045f0ba8bb4b463fd358f63f96d001710018
Mykola Kobets <mykola_kobets@epam.com>
2026-01-20 13:35:34 +0200

    cm: database: persist missing fields in launcher::InstanceInfo
    Signed-off-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

--- src/cm/database/database.hpp
+++ src/cm/database/database.hpp
@@ -322,10 +322,14 @@ private:
         eGID,
         eTimestamp,
         eState,
-        eIsUnitSubject
+        eIsUnitSubject,
+        eVersion,
+        eOwnerID,
+        eSubjectType
     };
-    using LauncherInstanceInfoRow = Poco::Tuple<std::string, std::string, uint64_t, std::string, std::string,
-        std::string, std::string, std::string, uint32_t, uint32_t, uint64_t, std::string, bool>;
+    using LauncherInstanceInfoRow
+        = Poco::Tuple<std::string, std::string, uint64_t, std::string, std::string, std::string, std::string,
+            std::string, uint32_t, uint32_t, uint64_t, std::string, bool, std::string, std::string, std::string>;
 
     enum class ImageManagerItemInfoColumns : int { eItemID = 0, eVersion, eIndexDigest, eState, eTimestamp };
     using ImageManagerItemInfoRow = Poco::Tuple<std::string, std::string, std::string, int, uint64_t>;
