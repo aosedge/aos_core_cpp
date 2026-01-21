12993ec50fdca509613d36360bceb1e2ed18ee11
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2026-01-21 21:56:55 +0200

    sm: launcher: container: add instance disk usage
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/sm/launcher/runtimes/container/itf/monitoring.hpp
+++ src/sm/launcher/runtimes/container/itf/monitoring.hpp
@@ -8,6 +8,7 @@
 #define AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_ITF_MONITORING_HPP_
 
 #include <string>
+#include <vector>
 
 #include <core/common/monitoring/itf/monitoringdata.hpp>
 
@@ -27,9 +28,13 @@ public:
      * Starts instance monitoring.
      *
      * @param instanceID instance ID.
+     * @param uid instance user ID.
+     * @param partInfos partition infos.
      * @return Error.
      */
-    virtual Error StartInstanceMonitoring(const std::string& instanceID) = 0;
+    virtual Error StartInstanceMonitoring(
+        const std::string& instanceID, uid_t uid, const std::vector<PartitionInfo>& partInfos)
+        = 0;
 
     /**
      * Stops instance monitoring.
