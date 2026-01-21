df81d96ffdd9b130142807dce2583a075f244684
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2026-01-21 20:48:41 +0200

    sm: launcher: container: add instance CPU and RAM monitoring
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/sm/launcher/runtimes/container/monitoring.hpp
+++ src/sm/launcher/runtimes/container/monitoring.hpp
@@ -7,6 +7,8 @@
 #ifndef AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_MONITORING_HPP_
 #define AOS_SM_LAUNCHER_RUNTIMES_CONTAINER_MONITORING_HPP_
 
+#include <unordered_map>
+
 #include "itf/monitoring.hpp"
 
 namespace aos::sm::launcher {
@@ -16,6 +18,11 @@ namespace aos::sm::launcher {
  */
 class Monitoring : public MonitoringItf {
 public:
+    /**
+     * Constructor.
+     */
+    Monitoring();
+
     /**
      * Starts instance monitoring.
      *
@@ -41,6 +48,24 @@ public:
      */
     Error GetInstanceMonitoringData(
         const std::string& instanceID, monitoring::InstanceMonitoringData& monitoringData) override;
+
+private:
+    static constexpr auto cCgroupsPath  = "/sys/fs/cgroup/system.slice/system-aos\\x2dservice.slice";
+    static constexpr auto cCpuUsageFile = "cpu.stat";
+    static constexpr auto cMemUsageFile = "memory.current";
+
+    struct CPUUsage {
+        size_t    mIdle      = 0;
+        size_t    mTotal     = 0;
+        aos::Time mTimestamp = Time::Now();
+    };
+
+    double GetInstanceCPUUsage(const std::string& instanceID);
+    size_t GetInstanceCPUUSec(const std::string& instanceID);
+    size_t GetInstanceRAMUsage(const std::string& instanceID);
+
+    size_t                                    mCPUCount;
+    std::unordered_map<std::string, CPUUsage> mInstanceMonitoringCache;
 };
 
 } // namespace aos::sm::launcher
