fe3dd589b0a0704f4ebee12685b525bafaa94ba3
Mykola Solianko <mykola_solianko@epam.com>
2026-01-21 14:50:48 +0200

    common: iamclient: add RegisterNode support to PublicNodesService
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>

--- src/common/iamclient/publicnodeservice.hpp
+++ src/common/iamclient/publicnodeservice.hpp
@@ -7,10 +7,15 @@
 #ifndef AOS_COMMON_IAMCLIENT_PUBLICNODESERVICE_HPP_
 #define AOS_COMMON_IAMCLIENT_PUBLICNODESERVICE_HPP_
 
+#include <atomic>
+#include <condition_variable>
+#include <functional>
 #include <memory>
 #include <mutex>
 #include <string>
+#include <thread>
 
+#include <grpcpp/grpcpp.h>
 #include <iamanager/v6/iamanager.grpc.pb.h>
 
 #include <common/utils/grpcsubscriptionmanager.hpp>
@@ -37,13 +42,15 @@ public:
 
     /**
      * Initializes public nodes service.
-     * @param iamPublicServerURL IAM public server URL.
+     * @param iamServerURL IAM server URL.
      * @param tlsCredentials TLS credentials.
      * @param insecureConnection whether to use insecure connection.
+     * @param publicServer whether to use public connection.
+     * @param certStorage certificate storage.
      * @return Error.
      */
-    Error Init(
-        const std::string& iamPublicServerURL, TLSCredentialsItf& tlsCredentials, bool insecureConnection = false);
+    Error Init(const std::string& iamServerURL, TLSCredentialsItf& tlsCredentials, bool insecureConnection = false,
+        bool publicServer = true, const std::string& certStorage = "");
 
     /**
      * Returns ids for all the nodes of the unit.
@@ -86,16 +93,60 @@ public:
      */
     Error Reconnect();
 
+    /**
+     * Start node registration.
+     *
+     * @return Error.
+     */
+    Error Start();
+
+    /**
+     * Stop node registration.
+     */
+    void Stop();
+
+    /**
+     * Send message.
+     *
+     * @param message Message.
+     * @return Error.
+     */
+    Error SendMessage(const iamanager::v6::IAMOutgoingMessages& message);
+
+protected:
+    virtual Error ReceiveMessage(const iamanager::v6::IAMIncomingMessages& msg);
+    virtual void  OnConnected();
+    virtual void  OnDisconnected();
+
 private:
-    static constexpr auto cServiceTimeout = std::chrono::seconds(10);
+    static constexpr auto cServiceTimeout    = std::chrono::seconds(10);
+    static constexpr auto cReconnectInterval = std::chrono::seconds(3);
+
+    void                                                    ConnectionLoop();
+    Error                                                   RegisterNode();
+    Error                                                   HandleIncomingMessage();
+    RetWithError<std::shared_ptr<grpc::ChannelCredentials>> CreateCredential();
 
     std::string                                                 mIAMPublicServerURL;
     bool                                                        mInsecureConnection {false};
+    bool                                                        mPublicServer {true};
+    std::string                                                 mCertStorage;
     std::shared_ptr<grpc::ChannelCredentials>                   mCredentials;
     std::unique_ptr<iamanager::v6::IAMPublicNodesService::Stub> mStub;
     TLSCredentialsItf*                                          mTLSCredentials {};
     mutable std::mutex                                          mMutex;
     std::unique_ptr<NodeInfoSubscriptionManager>                mSubscriptionManager;
+
+    std::unique_ptr<grpc::ClientContext> mRegisterNodeCtx;
+    std::unique_ptr<
+        grpc::ClientReaderWriterInterface<iamanager::v6::IAMOutgoingMessages, iamanager::v6::IAMIncomingMessages>>
+                mStream;
+    std::thread mConnectionThread;
+
+    std::atomic<bool>       mStop {false};
+    bool                    mConnected {false};
+    bool                    mStart {false};
+    std::condition_variable mCV;
 };
 
 } // namespace aos::common::iamclient
