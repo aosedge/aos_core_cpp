124b4ad52cf41e1a028d2a5a51f022d53cda41ac
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2026-02-19 20:29:01 +0200

    common: utils: use changed param oreder in FSPlatform::SetUserQuota
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/common/utils/fsplatform.hpp
+++ src/common/utils/fsplatform.hpp
@@ -49,11 +49,11 @@ public:
      * Sets user quota for the given path.
      *
      * @param path path to set quota for.
-     * @param quota quota size in bytes.
      * @param uid user ID.
+     * @param quota quota size in bytes.
      * @return Error.
      */
-    Error SetUserQuota(const String& path, size_t quota, size_t uid) const override;
+    Error SetUserQuota(const String& path, uid_t uid, size_t quota) const override;
 
     /**
      * Changes the owner of a file or directory.
