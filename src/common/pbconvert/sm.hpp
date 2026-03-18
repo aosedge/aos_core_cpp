d9b44c35d5d73e6394f18f51717b70340a6d09b4
Mykola Solianko <mykola_solianko@epam.com>
2026-03-18 17:07:45 +0200

    common: pbconvert: rename network types
    - NetworkParameters -> NetworkParams
    - InstanceNetworkParameters -> InstanceNetworkAllocation
    - NetworkServiceData -> UpdateItemNetworkParams
    
    Signed-off-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>

--- src/common/pbconvert/sm.hpp
+++ src/common/pbconvert/sm.hpp
@@ -7,6 +7,7 @@
 #ifndef AOS_COMMON_PBCONVERT_SM_HPP_
 #define AOS_COMMON_PBCONVERT_SM_HPP_
 
+#include <servicemanager/v5/network.grpc.pb.h>
 #include <servicemanager/v5/servicemanager.grpc.pb.h>
 
 #include <core/cm/nodeinfoprovider/itf/sminforeceiver.hpp>
@@ -46,7 +47,7 @@ Error ConvertFromProto(const servicemanager::v5::NodeConfigStatus& grpcStatus, N
  *
  * @param config node config.
  * @param result check node config message.
- * @return Error
+ * @return Error.
  */
 Error ConvertToProto(const NodeConfig& config, servicemanager::v5::CheckNodeConfig& result);
 
@@ -55,7 +56,7 @@ Error ConvertToProto(const NodeConfig& config, servicemanager::v5::CheckNodeConf
  *
  * @param config node config.
  * @param result check node config message.
- * @return Error
+ * @return Error.
  */
 Error ConvertToProto(const NodeConfig& config, servicemanager::v5::SetNodeConfig& result);
 
@@ -65,7 +66,7 @@ Error ConvertToProto(const NodeConfig& config, servicemanager::v5::SetNodeConfig
  * @param grpcAlert grpc alert.
  * @param nodeID node ID.
  * @param alertItem Aos alert item.
- * @return Error
+ * @return Error.
  */
 Error ConvertFromProto(const servicemanager::v5::Alert& grpcAlert, const String& nodeID, AlertVariant& alertItem);
 
@@ -74,7 +75,7 @@ Error ConvertFromProto(const servicemanager::v5::Alert& grpcAlert, const String&
  *
  * @param log Aos request log.
  * @param result grpc system log request.
- * @return Error
+ * @return Error.
  */
 Error ConvertToProto(const RequestLog& log, servicemanager::v5::SystemLogRequest& result);
 
@@ -83,7 +84,7 @@ Error ConvertToProto(const RequestLog& log, servicemanager::v5::SystemLogRequest
  *
  * @param log Aos request log.
  * @param result grpc instance log request.
- * @return Error
+ * @return Error.
  */
 Error ConvertToProto(const RequestLog& log, servicemanager::v5::InstanceLogRequest& result);
 
@@ -92,7 +93,7 @@ Error ConvertToProto(const RequestLog& log, servicemanager::v5::InstanceLogReque
  *
  * @param log Aos request log.
  * @param result grpc instance crash log request.
- * @return Error
+ * @return Error.
  */
 Error ConvertToProto(const RequestLog& log, servicemanager::v5::InstanceCrashLogRequest& result);
 
@@ -101,7 +102,7 @@ Error ConvertToProto(const RequestLog& log, servicemanager::v5::InstanceCrashLog
  *
  * @param src grpc system log request.
  * @param[out] dst Aos request log.
- * @return Error
+ * @return Error.
  */
 Error ConvertFromProto(const servicemanager::v5::SystemLogRequest& src, RequestLog& dst);
 
@@ -110,7 +111,7 @@ Error ConvertFromProto(const servicemanager::v5::SystemLogRequest& src, RequestL
  *
  * @param src grpc instance log request.
  * @param[out] dst Aos request log.
- * @return Error
+ * @return Error.
  */
 Error ConvertFromProto(const servicemanager::v5::InstanceLogRequest& src, RequestLog& dst);
 
@@ -119,7 +120,7 @@ Error ConvertFromProto(const servicemanager::v5::InstanceLogRequest& src, Reques
  *
  * @param src grpc instance crash log request.
  * @param[out] dst Aos request log.
- * @return Error
+ * @return Error.
  */
 Error ConvertFromProto(const servicemanager::v5::InstanceCrashLogRequest& src, RequestLog& dst);
 
@@ -129,18 +130,9 @@ Error ConvertFromProto(const servicemanager::v5::InstanceCrashLogRequest& src, R
  * @param grpcLogData grpc log data.
  * @param nodeID node ID.
  * @param aosPushLog Aos push log.
- * @return Error
+ * @return Error.
  */
 Error ConvertFromProto(const servicemanager::v5::LogData& grpcLogData, const String& nodeID, PushLog& aosPushLog);
-
-/**
- * Converts Aos array of update network parameters to grpc update networks message.
- *
- * @param networkParams Aos array of update network parameters.
- * @param result grpc update networks message.
- * @return Error
- */
-Error ConvertToProto(const Array<UpdateNetworkParameters>& networkParams, servicemanager::v5::UpdateNetworks& result);
 
 /**
  * Converts Aos instance info arrays to grpc update instances message.
@@ -148,7 +140,7 @@ Error ConvertToProto(const Array<UpdateNetworkParameters>& networkParams, servic
  * @param stopInstances Aos instances to stop.
  * @param startInstances Aos instances to start.
  * @param result grpc update instances message.
- * @return Error
+ * @return Error.
  */
 Error ConvertToProto(const Array<aos::InstanceInfo>& stopInstances, const Array<aos::InstanceInfo>& startInstances,
     servicemanager::v5::UpdateInstances& result);
@@ -159,19 +151,10 @@ Error ConvertToProto(const Array<aos::InstanceInfo>& stopInstances, const Array<
  * @param src grpc update instances.
  * @param[out] stopInstances Aos instances to stop.
  * @param[out] startInstances Aos instances to start.
- * @return Error
+ * @return Error.
  */
 Error ConvertFromProto(const servicemanager::v5::UpdateInstances& src, Array<InstanceIdent>& stopInstances,
     Array<InstanceInfo>& startInstances);
-
-/**
- * Converts grpc update networks to Aos network parameters.
- *
- * @param src grpc update networks.
- * @param[out] dst Aos network parameters array.
- * @return Error
- */
-Error ConvertFromProto(const servicemanager::v5::UpdateNetworks& src, Array<NetworkParameters>& dst);
 
 /**
  * Converts grpc average monitoring to Aos node monitoring data.
@@ -179,7 +162,7 @@ Error ConvertFromProto(const servicemanager::v5::UpdateNetworks& src, Array<Netw
  * @param src grpc average monitoring.
  * @param nodeID node ID.
  * @param dst Aos node monitoring data.
- * @return Error
+ * @return Error.
  */
 Error ConvertFromProto(
     const servicemanager::v5::AverageMonitoring& src, const String& nodeID, aos::monitoring::NodeMonitoringData& dst);
@@ -190,7 +173,7 @@ Error ConvertFromProto(
  * @param src grpc instance status.
  * @param nodeID node ID to set in the result.
  * @param dst Aos instance status.
- * @return Error
+ * @return Error.
  */
 Error ConvertFromProto(const servicemanager::v5::InstanceStatus& src, const String& nodeID, aos::InstanceStatus& dst);
 
@@ -200,7 +183,7 @@ Error ConvertFromProto(const servicemanager::v5::InstanceStatus& src, const Stri
  * @param src grpc instant monitoring.
  * @param nodeID node ID to set in the result.
  * @param dst Aos node monitoring data.
- * @return Error
+ * @return Error.
  */
 Error ConvertFromProto(
     const servicemanager::v5::InstantMonitoring& src, const String& nodeID, aos::monitoring::NodeMonitoringData& dst);
@@ -210,7 +193,7 @@ Error ConvertFromProto(
  *
  * @param src grpc SM info.
  * @param dst Aos SM info.
- * @return Error
+ * @return Error.
  */
 Error ConvertFromProto(const servicemanager::v5::SMInfo& src, aos::cm::nodeinfoprovider::SMInfo& dst);
 
@@ -286,6 +269,33 @@ void ConvertToProto(const PushLog& src, servicemanager::v5::LogData& dst);
  * @param[out] dst protobuf alert.
  */
 void ConvertToProto(const AlertVariant& src, servicemanager::v5::Alert& dst);
+
+/**
+ * Converts grpc UpdateItemNetworkParams to Aos.
+ *
+ * @param src grpc network service data.
+ * @param[out] dst Aos network service data.
+ * @return Error.
+ */
+Error ConvertFromProto(const servicemanager::v5::UpdateItemNetworkParams& src, UpdateItemNetworkParams& dst);
+
+/**
+ * Converts Aos network parameters to grpc GetNodeNetworkParamsResponse.
+ *
+ * @param src Aos network parameters.
+ * @param[out] dst grpc response.
+ * @return Error.
+ */
+Error ConvertToProto(const NetworkParams& src, servicemanager::v5::GetNodeNetworkParamsResponse& dst);
+
+/**
+ * Converts Aos instance network parameters to grpc AllocateInstanceNetworkResponse.
+ *
+ * @param src Aos instance network parameters.
+ * @param[out] dst grpc response.
+ * @return Error.
+ */
+Error ConvertToProto(const InstanceNetworkAllocation& src, servicemanager::v5::AllocateInstanceNetworkResponse& dst);
 
 } // namespace aos::common::pbconvert
 
