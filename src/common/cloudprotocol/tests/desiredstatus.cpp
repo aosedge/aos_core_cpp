8486288dfa3d4b573742a61adfcdc44a13a32214
Oleksandr Grytsov <oleksandr_grytsov@epam.com>
2026-03-23 16:07:53 +0200

    common: cloudprotocol: rename "item" to "identity" desired status
    Rename "item" to "identity" in desired status "items" array.
    
    Signed-off-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>

--- src/common/cloudprotocol/tests/desiredstatus.cpp
+++ src/common/cloudprotocol/tests/desiredstatus.cpp
@@ -231,7 +231,7 @@ TEST_F(CloudProtocolDesiredStatus, Items)
         "messageType": "desiredStatus",
         "items": [
             {
-                "item": {
+                "identity": {
                     "id": "item1",
                     "type": "service"
                 },
@@ -242,7 +242,7 @@ TEST_F(CloudProtocolDesiredStatus, Items)
                 "indexDigest": "sha256:36f028580bb02cc8272a9a020f4200e346e276ae664e45ee80745574e2f5ab80"
             },
             {
-                "item": {
+                "identity": {
                     "id": "item2",
                     "type": "component"
                 },
