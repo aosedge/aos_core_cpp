710d244e51ca85aed0a64a62d68fc0c36de5ae35
Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
2026-06-03 11:31:08 +0300

    sm: launcher: runtimes: container: remove hosts config field
    Resource manager is responsible for providing desired hosts
    configuration to the container runtime, so the hosts field is
    no longer needed in the runtime configuration.
    
    Signed-off-by: Mykhailo Lohvynenko <mykhailo_lohvynenko@epam.com>
    Reviewed-by: Mykola Kobets <mykola_kobets@epam.com>
    Reviewed-by: Mykola Solianko <mykola_solianko@epam.com>
    Reviewed-by: Oleksandr Grytsov <oleksandr_grytsov@epam.com>

--- src/sm/launcher/runtimes/container/config.cpp
+++ src/sm/launcher/runtimes/container/config.cpp
@@ -11,28 +11,6 @@
 
 namespace aos::sm::launcher {
 
-namespace {
-
-/***********************************************************************************************************************
- * static
- **********************************************************************************************************************/
-
-Host ParseHostConfig(const common::utils::CaseInsensitiveObjectWrapper& object)
-{
-    Host  host;
-    Error err;
-
-    err = host.mIP.Assign(object.GetValue<std::string>("ip").c_str());
-    AOS_ERROR_CHECK_AND_THROW(err);
-
-    err = host.mHostname.Assign(object.GetValue<std::string>("hostname").c_str());
-    AOS_ERROR_CHECK_AND_THROW(err);
-
-    return host;
-}
-
-} // namespace
-
 /***********************************************************************************************************************
  * Public
  **********************************************************************************************************************/
@@ -46,12 +24,6 @@ void ParseContainerConfig(
     config.mStorageDir = object.GetValue<std::string>("storageDir", common::utils::JoinPath(workingDir, "storages"));
     config.mStateDir   = object.GetValue<std::string>("stateDir", common::utils::JoinPath(workingDir, "states"));
     config.mHostBinds  = common::utils::GetArrayValue<std::string>(object, "hostBinds");
-
-    const auto hosts = common::utils::GetArrayValue<Host>(object, "hosts",
-        [](const auto& val) { return ParseHostConfig(common::utils::CaseInsensitiveObjectWrapper(val)); });
-    for (const auto& host : hosts) {
-        config.mHosts.push_back(host);
-    }
 }
 
 } // namespace aos::sm::launcher
